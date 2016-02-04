233513a68c95827484f72b8049f41f2f3208b26a
Burton DeWilde <burton@chartbeat.com>
2016-02-04 12:30:52 -0500

    added preprocess module

---
+++ textacy/preprocess.py
@@ -0,0 +1,222 @@
+# -*- coding: utf-8 -*-
+"""
+Functions that modify raw text *in-place*, replacing contractions, URLs, emails,
+phone numbers, and currency symbols with standardized forms. These should be
+applied before processing by spacy (http://spacy.io), but be warned: preprocessing
+may affect the interpretation of the text -- and spacy's processing of it.
+"""
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import re
+import unicodedata
+
+from ftfy import fix_text
+from unidecode import unidecode
+
+from ds_tools.nlp.regexes_etc import (CURRENCIES, URL_REGEX, SHORT_URL_REGEX, EMAIL_REGEX,
+                                      PHONE_REGEX, NUMBERS_REGEX, PUNCT_REGEX, CURRENCY_REGEX,
+                                      LINEBREAK_REGEX, NONBREAKING_SPACE_REGEX)
+
+
+def fix_bad_unicode(text, normalization='NFC'):
+    """
+    Fix unicode text that's "broken" using ftfy (http://ftfy.readthedocs.org/);
+    this includes mojibake, HTML entities and other code cruft,
+    and non-standard forms for display purposes.
+
+    Args:
+        text (str): raw text
+        normalization (str {'NFC', 'NFKC', 'NFD', 'NFKD'}, optional): if 'NFC',
+            combines characters and diacritics written using separate code points,
+            e.g. converting "e" plus an acute accent modifier into "é"; unicode
+            can be converted to NFC form without any change in its meaning!
+            if 'NFKC', additional normalizations are applied that can change
+            the meanings of characters, e.g. ellipsis characters will be replaced
+            with three periods
+
+    Returns:
+        str
+    """
+    return fix_text(text, normalization=normalization)
+
+
+def transliterate_unicode(text):
+    """
+    Try to represent unicode data in ascii characters similar to what a human
+    with a US keyboard would choose.
+
+    Works great for languages of Western origin, worse the farther the language
+    gets from Latin-based alphabets. It's based on hand-tuned character mappings
+    that also contain ascii approximations for symbols and non-Latin alphabets.
+    """
+    return unidecode(text)
+
+
+def normalize_whitespace(text):
+    """
+    Given str `text`, replace one or more spacings with a single space, and one
+    or more linebreaks with a single newline. Also strip leading/trailing whitespace.
+    """
+    return NONBREAKING_SPACE_REGEX.sub(' ', LINEBREAK_REGEX.sub(r'\n', text)).strip()
+
+
+def unpack_contractions(text):
+    """
+    Replace *English* contractions in `text` with their unshortened forms.
+    N.B. The "'d" and "'s" forms are ambiguous (had/would, is/has/possessive),
+    so are left as-is.
+    """
+    # standard
+    text = re.sub(r"(\b)([Aa]re|[Cc]ould|[Dd]id|[Dd]oes|[Dd]o|[Hh]ad|[Hh]as|[Hh]ave|[Ii]s|[Mm]ight|[Mm]ust|[Ss]hould|[Ww]ere|[Ww]ould)n't", r"\1\2 not", text)
+    text = re.sub(r"(\b)([Hh]e|[Ii]|[Ss]he|[Tt]hey|[Ww]e|[Ww]hat|[Ww]ho|[Yy]ou)'ll", r"\1\2 will", text)
+    text = re.sub(r"(\b)([Tt]hey|[Ww]e|[Ww]hat|[Ww]ho|[Yy]ou)'re", r"\1\2 are", text)
+    text = re.sub(r"(\b)([Ii]|[Ss]hould|[Tt]hey|[Ww]e|[Ww]hat|[Ww]ho|[Ww]ould|[Yy]ou)'ve", r"\1\2 have", text)
+    # non-standard
+    text = re.sub(r"(\b)([Cc]a)n't", r"\1\2n not", text)
+    text = re.sub(r"(\b)([Ii])'m", r"\1\2 am", text)
+    text = re.sub(r"(\b)([Ll]et)'s", r"\1\2 us", text)
+    text = re.sub(r"(\b)([Ww])on't", r"\1\2ill not", text)
+    text = re.sub(r"(\b)([Ss])han't", r"\1\2hall not", text)
+    text = re.sub(r"(\b)([Yy])(?:'all|a'll)", r"\1\2ou all", text)
+    return text
+
+
+def replace_urls(text, replace_with='*URL*'):
+    """Replace all URLs in str `text` with str `replace_with`."""
+    return URL_REGEX.sub(replace_with, SHORT_URL_REGEX.sub(replace_with, text))
+
+
+def replace_emails(text, replace_with='*EMAIL*'):
+    """Replace all emails in str `text` with str `replace_with`."""
+    return EMAIL_REGEX.sub(replace_with, text)
+
+
+def replace_phone_numbers(text, replace_with='*PHONE*'):
+    """Replace all phone numbers in str `text` with str `replace_with`."""
+    return PHONE_REGEX.sub(replace_with, text)
+
+
+def replace_numbers(text, replace_with='*NUMBER*'):
+    """Replace all numbers in str `text` with str `replace_with`."""
+    return NUMBERS_REGEX.sub(replace_with, text)
+
+
+def remove_punct(text):
+    """Remove all punctuation from str `text` (replace punct marks with empty string)."""
+    return PUNCT_REGEX.sub('', text)
+
+
+def replace_currency_symbols(text, replace_with=None):
+    """
+    Replace all currency symbols in str `text` with string specified by `replace_with`.
+
+    Args:
+        text (str): raw text
+        replace_with (str, optional): if None (default), replace symbols with
+            their standard 3-letter abbreviations (e.g. '$' with 'USD', '£' with 'GBP');
+            otherwise, pass in a string with which to replace all symbols
+            (e.g. "*CURRENCY*")
+
+    Returns:
+        str
+    """
+    if replace_with is None:
+        for k, v in CURRENCIES.items():
+            text = text.replace(k, v)
+        return text
+    else:
+        return CURRENCY_REGEX.sub(replace_with, text)
+
+
+def remove_accents(text, method='unicode'):
+    """
+    Remove accents from any accented unicode characters in str `text`, either by
+    transforming them into ascii equivalents or removing them entirely.
+
+    Args:
+        text (str): raw text
+        method (str {'unicode', 'ascii'}, optional): if 'unicode', remove accented
+            char for any unicode symbol with a direct ASCII equivalent; if 'ascii',
+            remove accented char for any unicode symbol
+
+            NB: the 'ascii' method is notably faster than 'unicode', but less good
+
+    Returns:
+        str
+
+    Raises:
+        ValueError: if `method` is not in {'unicode', 'ascii'}
+    """
+    if method == 'unicode':
+        return ''.join(c for c in unicodedata.normalize('NFKD', text)
+                       if not unicodedata.combining(c))
+    elif method == 'ascii':
+        return unicodedata.normalize('NFKD', text).encode('ascii', errors='ignore').decode('ascii')
+    else:
+        msg = '`method` must be either "unicode" and "ascii", not {}'.format(method)
+        raise ValueError(msg)
+
+
+def preprocess_text(text, fix_unicode=False, lowercase=False, transliterate=False,
+                    no_urls=False, no_emails=False, no_phone_numbers=False,
+                    no_numbers=False, no_currency_symbols=False, no_punct=False,
+                    no_contractions=False, no_accents=False):
+    """
+    Normalize various aspects of a raw text doc before parsing it with Spacy.
+    A convenience function for applying all other preprocessing functions in one go.
+
+    WARNING: These changes may negatively affect subsequent NLP analysis performed
+    on the text, so choose carefully, and preprocess at your own risk!
+
+    Args:
+        text (str): raw text to preprocess
+        fix_unicode (bool, optional): if True, fix "broken" unicode such as
+            mojibake and garbled HTML entities
+        lowercase (bool, optional): if True, all text is lower-cased
+        transliterate (bool, optional): if True, convert non-ascii characters
+            into their closest ascii equivalents
+        no_urls (bool, optional): if True, replace all URL strings with '*URL*'
+        no_emails (bool, optional): if True, replace all email strings with '*EMAIL*'
+        no_phone_numbers (bool, optional): if True, replace all phone number strings
+            with '*PHONE*'
+        no_numbers (bool, optional): if True, replace all number-like strings
+            with '*NUMBER*'
+        no_currency_symbols (bool, optional): if True, replace all currency symbols
+            with their standard 3-letter abbreviations
+        no_punct (bool, optional): if True, remove all punctuation (replace with
+            empty string)
+        no_contractions (bool, optional): if True, replace *English* contractions
+            with their unshortened forms
+        no_accents (bool, optional): if True, replace all accented characters
+            with unaccented versions; NB: if `transliterate` is True, this option
+            is redundant
+
+    Returns:
+        str: input `text` processed according to function args
+    """
+    if fix_unicode is True:
+        text = fix_bad_unicode(text, normalization='NFC')
+    if transliterate is True:
+        text = transliterate_unicode(text)
+    if no_urls is True:
+        text = replace_urls(text)
+    if no_emails is True:
+        text = replace_emails(text)
+    if no_phone_numbers is True:
+        text = replace_phone_numbers(text)
+    if no_numbers is True:
+        text = replace_numbers(text)
+    if no_currency_symbols is True:
+        text = replace_currency_symbols(text)
+    if no_contractions is True:
+        text = unpack_contractions(text)
+    if no_accents is True:
+        text = remove_accents(text, method='unicode')
+    if no_punct is True:
+        text = remove_punct(text)
+    if lowercase is True:
+        text = text.lower()
+    # always normalize whitespace; treat linebreaks separately from spacing
+    text = normalize_whitespace(text)
+
+    return text
