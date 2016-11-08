36b40a657e4e1fa8230c87f61b95ca319510d538
Burton DeWilde <burton@chartbeat.com>
2016-11-08 09:21:14 -0500

    gensim export function no longer depends on gensim
    - `docs_to_gensim` function now returns a string formatted such that it
    can be easily loaded into a gensim dictionary, rather than a gensim
    dictionary itself; i’m trying to drop the gensim dependency…
    - dropped the single doc-to-gensim function, since that’s basically
    never how one works with gensim

--- textacy/export.py
+++ textacy/export.py
@@ -4,99 +4,62 @@ Module for exporting textacy/spacy objects into "third-party" formats.
 from collections import Counter
 from operator import itemgetter
 
-from gensim.corpora.dictionary import Dictionary
 from spacy import attrs
 from spacy.strings import StringStore
 
-from textacy import extract
 
-
-def doc_to_gensim(doc, lemmatize=True,
-                  filter_stops=True, filter_punct=True, filter_nums=False):
-    """
-    Convert a single ``spacy.Doc`` into a gensim dictionary and bag-of-words document.
-
-    Args:
-        doc (``spacy.Doc``)
-        lemmatize (bool): if True, use lemmatized strings for words; otherwise,
-            use the original form of the string as it appears in ``doc``
-        filter_stops (bool): if True, remove stop words from word list
-        filter_punct (bool): if True, remove punctuation from word list
-        filter_nums (bool): if True, remove numbers from word list
-
-    Returns:
-        :class:`gensim.Dictionary <gensim.corpora.dictionary.Dictionary>`:
-            integer word ID to word string mapping
-        list((int, int)): bag-of-words document, a list of (integer word ID, word count)
-            2-tuples
-    """
-    gdict = Dictionary()
-    words = extract.words(doc,
-                          filter_stops=filter_stops,
-                          filter_punct=filter_punct,
-                          filter_nums=filter_nums)
-    if lemmatize is True:
-        gdoc = gdict.doc2bow((word.lemma_ for word in words), allow_update=True)
-    else:
-        gdoc = gdict.doc2bow((word.orth_ for word in words), allow_update=True)
-
-    return (gdict, gdoc)
-
-
-def docs_to_gensim(spacy_docs, spacy_vocab, lemmatize=True,
+def docs_to_gensim(spacy_docs, spacy_vocab, lemmatize=True, lowercase=False,
                    filter_stops=True, filter_punct=True, filter_nums=False):
     """
-    Convert multiple ``spacy.Doc`` s into a gensim dictionary and bag-of-words corpus.
+    Convert a sequence of ``spacy.Doc`` s into a gensim-friendly corpus and a
+    string that can be loaded into a :class:`gensim.corpora.Dictionary`.
 
     Args:
-        spacy_docs (list(``spacy.Doc``))
+        spacy_docs (Iterable[``spacy.Doc``])
         spacy_vocab (``spacy.Vocab``)
-        lemmatize (bool): if True, use lemmatized strings for words; otherwise,
-            use the original form of the string as it appears in ``doc``
+        lemmatize (bool): if True, use lemmatized strings for words
+        lowercase (bool): if True (and ``lemmatize`` is False), use lowercased
+            strings for words
         filter_stops (bool): if True, remove stop words from word list
         filter_punct (bool): if True, remove punctuation from word list
         filter_nums (bool): if True, remove numbers from word list
 
     Returns:
-        :class:`gensim.Dictionary <gensim.corpora.dictionary.Dictionary>`:
-            integer word ID to word string mapping
-        list(list((int, int))): list of bag-of-words documents, where each doc is
-            a list of (integer word ID, word count) 2-tuples
+        str: words, their integer ids, and their document frequencies in
+            ``spacy_docs``, as a string formatted like `id[TAB]word[TAB]df[NEWLINE]`;
+            when written to file, can be converted into a gensim ``Dictionary``
+            via :meth:`gensim.corpora.Dictionary.load_from_text()`
+        List[List[Tuple[int, int]]]: list of documents as bags-of-words, where
+            each doc is a list of (integer word ID, word count) 2-tuples
     """
-    gdict = Dictionary()
+    count_by = (attrs.LEMMA if lemmatize is True else
+                attrs.LOWER if lowercase is True else
+                attrs.ORTH)
     gcorpus = []
     stringstore = StringStore()
     doc_freqs = Counter()
 
     for spacy_doc in spacy_docs:
-        if lemmatize is True:
-            bow = ((spacy_vocab[tok_id], count)
-                   for tok_id, count in spacy_doc.count_by(attrs.LEMMA).items())
-        else:
-            bow = ((spacy_vocab[tok_id], count)
-                   for tok_id, count in spacy_doc.count_by(attrs.ORTH).items())
-
+        bow = ((spacy_vocab[tok_id], count)
+               for tok_id, count in spacy_doc.count_by(count_by).items())
+        bow = ((lex, count) for lex, count in bow if not lex.is_space)
         if filter_stops is True:
             bow = ((lex, count) for lex, count in bow if not lex.is_stop)
         if filter_punct is True:
             bow = ((lex, count) for lex, count in bow if not lex.is_punct)
         if filter_nums is True:
             bow = ((lex, count) for lex, count in bow if not lex.like_num)
-
         bow = sorted(((stringstore[lex.orth_], count) for lex, count in bow),
                      key=itemgetter(0))
 
         doc_freqs.update(tok_id for tok_id, _ in bow)
-        gdict.num_docs += 1
-        gdict.num_pos += sum(count for _, count in bow)
-        gdict.num_nnz += len(bow)
-
         gcorpus.append(bow)
 
-    gdict.token2id = {s: i for i, s in enumerate(stringstore)}
-    gdict.dfs = dict(doc_freqs)
+    gdict_str = '\n'.join(
+        '{}\t{}\t{}'.format(i, s, doc_freqs[i])
+        for i, s in sorted(enumerate(stringstore), key=itemgetter(1)))
 
-    return (gdict, gcorpus)
+    return (gdict_str, gcorpus)
 
 
 def doc_to_conll(doc):
