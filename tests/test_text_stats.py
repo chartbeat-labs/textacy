3ecb1b5fabadf3c612c882d4027123dc6915957e
Burton DeWilde <burton@chartbeat.com>
2017-04-10 21:32:23 -0400

    Added LIX statistic, better TextStats docs
    - Added deprecation warning message to `readability_stats()` function
    - Updated text stats tests

--- tests/test_text_stats.py
+++ tests/test_text_stats.py
@@ -5,81 +5,157 @@ import unittest
 from textacy import text_stats
 from textacy import Doc
 
+TEXT = """
+Mr. Speaker, 480,000 Federal employees are working without pay, a form of involuntary servitude; 280,000 Federal employees are not working, and they will be paid. Virtually all of these workers have mortgages to pay, children to feed, and financial obligations to meet.
+Mr. Speaker, what is happening to these workers is immoral, is wrong, and must be rectified immediately. Newt Gingrich and the Republican leadership must not continue to hold the House and the American people hostage while they push their disastrous 7-year balanced budget plan. The gentleman from Georgia, Mr. Gingrich, and the Republican leadership must join Senator Dole and the entire Senate and pass a continuing resolution now, now to reopen Government.
+Mr. Speaker, that is what the American people want, that is what they need, and that is what this body must do.
+"""
+
 
 class TextStatsTestCase(unittest.TestCase):
+
     def setUp(self):
-        self.spacy_doc = Doc('This is an English-language document.')
-        self.n_chars = 2855
-        self.n_syllables = 857
-        self.n_words = 441
-        self.n_polysyllable_words = 104
-        self.n_monosyllable_words = 187
-        self.n_long_words = 205
-        self.n_sents = 21
+        self.doc = Doc(TEXT)
+        self.ts = text_stats.TextStats(self.doc)
 
-    def test_readability_stats(self):
-        self.assertIsInstance(text_stats.readability_stats(self.spacy_doc), dict)
+    def test_n_sents(self):
+        self.assertEqual(self.ts.n_sents, 6)
 
-    def test_readability_stats_lang(self):
-        self.spacy_doc.lang = 'es'
-        self.assertRaises(
-            NotImplementedError, text_stats.readability_stats, self.spacy_doc)
+    def test_n_words(self):
+        self.assertEqual(self.ts.n_words, 136)
+
+    def test_n_chars(self):
+        self.assertEqual(self.ts.n_chars, 685)
+
+    def test_n_syllables(self):
+        self.assertEqual(self.ts.n_syllables, 214)
+
+    def test_n_unique_words(self):
+        self.assertEqual(self.ts.n_unique_words, 80)
+
+    def test_n_long_words(self):
+        self.assertEqual(self.ts.n_long_words, 43)
+
+    def test_n_monosyllable_words(self):
+        self.assertEqual(self.ts.n_monosyllable_words, 90)
+
+    def test_n_polysyllable_words(self):
+        self.assertEqual(self.ts.n_polysyllable_words, 24)
 
     def test_flesch_kincaid_grade_level(self):
         self.assertAlmostEqual(
-            text_stats.flesch_kincaid_grade_level(self.n_syllables, self.n_words, self.n_sents),
-            15.53, places=2)
+            self.ts.flesch_kincaid_grade_level,
+            11.817647058823532,
+            places=2)
 
     def test_flesch_readability_ease(self):
         self.assertAlmostEqual(
-            text_stats.flesch_readability_ease(self.n_syllables, self.n_words, self.n_sents),
-            21.12, places=2)
+            self.ts.flesch_readability_ease,
+            50.707745098039254,
+            places=2)
 
     def test_smog_index(self):
         self.assertAlmostEqual(
-            text_stats.smog_index(self.n_polysyllable_words, self.n_sents, verbose=False),
-            15.84, places=2)
+            self.ts.smog_index,
+            14.554592549557764,
+            places=2)
 
     def test_gunning_fog_index(self):
         self.assertAlmostEqual(
-            text_stats.gunning_fog_index(self.n_words, self.n_polysyllable_words, self.n_sents),
-            17.83, places=2)
+            self.ts.gunning_fog_index,
+            16.12549019607843,
+            places=2)
 
     def test_coleman_liau_index(self):
         self.assertAlmostEqual(
-            text_stats.coleman_liau_index(self.n_chars, self.n_words, self.n_sents),
-            20.86, places=2)
+            self.ts.coleman_liau_index,
+            12.509300816176474,
+            places=2)
 
     def test_automated_readability_index(self):
         self.assertAlmostEqual(
-            text_stats.automated_readability_index(self.n_chars, self.n_words, self.n_sents),
-            19.56, places=2)
+            self.ts.automated_readability_index,
+            13.626495098039214,
+            places=2)
 
-    def test_wiener_sachtextformel_1(self):
+    def test_lix(self):
         self.assertAlmostEqual(
-            text_stats.wiener_sachtextformel(self.n_words, self.n_sents, self.n_polysyllable_words,
-                                             self.n_monosyllable_words, self.n_long_words, variant=1),
-            11.84, places=2)
-
-    def test_wiener_sachtextformel_2(self):
-        self.assertAlmostEqual(
-            text_stats.wiener_sachtextformel(self.n_words, self.n_sents, self.n_polysyllable_words,
-                                             self.n_monosyllable_words, self.n_long_words, variant=2),
-            11.87, places=2)
-
-    def test_wiener_sachtextformel_3(self):
-        self.assertAlmostEqual(
-            text_stats.wiener_sachtextformel(self.n_words, self.n_sents, self.n_polysyllable_words,
-                                             self.n_monosyllable_words, self.n_long_words, variant=3),
-            9.87, places=2)
-
-    def test_wiener_sachtextformel_4(self):
-        self.assertAlmostEqual(
-            text_stats.wiener_sachtextformel(self.n_words, self.n_sents, self.n_polysyllable_words,
-                                             self.n_monosyllable_words, self.n_long_words, variant=4),
-            10.36, places=2)
+            self.ts.lix,
+            54.28431372549019,
+            places=2)
 
     def test_gulpease_index(self):
         self.assertAlmostEqual(
-            text_stats.gulpease_index(self.n_chars, self.n_words, self.n_sents),
-            38.55, places=2)
+            self.ts.gulpease_index,
+            51.86764705882353,
+            places=2)
+
+    def test_wiener_sachtextformel(self):
+        self.assertAlmostEqual(
+            self.ts.wiener_sachtextformel,
+            8.266410784313727,
+            places=2)
+
+    def test_basic_counts(self):
+        self.assertIsInstance(self.ts.basic_counts, dict)
+        basic_counts = self.ts.basic_counts
+        basic_counts_keys = (
+            'n_sents', 'n_words', 'n_chars', 'n_syllables', 'n_unique_words',
+            'n_long_words', 'n_monosyllable_words', 'n_polysyllable_words')
+        for key in basic_counts_keys:
+            self.assertEqual(basic_counts[key], getattr(self.ts, key))
+
+    def test_readability_stats(self):
+        self.assertIsInstance(self.ts.basic_counts, dict)
+        readability_stats = self.ts.readability_stats
+        readability_stats_keys = (
+            'flesch_kincaid_grade_level', 'flesch_readability_ease', 'smog_index',
+            'gunning_fog_index', 'coleman_liau_index', 'automated_readability_index',
+            'lix', 'gulpease_index', 'wiener_sachtextformel')
+        for key in readability_stats_keys:
+            self.assertEqual(readability_stats[key], getattr(self.ts, key))
+
+    def test_readability_stats_function(self):
+        self.assertIsInstance(text_stats.readability_stats(self.doc), dict)
+
+    def test_readability_stats_lang_function(self):
+        self.doc.lang = 'es'
+        self.assertRaises(
+            NotImplementedError, text_stats.readability_stats, self.doc)
+
+    def test_wiener_sachtextformel_variant1(self):
+        self.assertEqual(
+            self.ts.wiener_sachtextformel,
+            text_stats.wiener_sachtextformel(
+                self.ts.n_words, self.ts.n_polysyllable_words, self.ts.n_monosyllable_words,
+                self.ts.n_long_words, self.ts.n_sents, variant=1))
+        self.assertAlmostEqual(
+            text_stats.wiener_sachtextformel(
+                self.ts.n_words, self.ts.n_polysyllable_words, self.ts.n_monosyllable_words,
+                self.ts.n_long_words, self.ts.n_sents, variant=1),
+            8.266410784313727,
+            places=2)
+
+    def test_wiener_sachtextformel_variant2(self):
+        self.assertAlmostEqual(
+            text_stats.wiener_sachtextformel(
+                self.ts.n_words, self.ts.n_polysyllable_words, self.ts.n_monosyllable_words,
+                self.ts.n_long_words, self.ts.n_sents, variant=2),
+            8.916400980392158,
+            places=2)
+
+    def test_wiener_sachtextformel_variant3(self):
+        self.assertAlmostEqual(
+            text_stats.wiener_sachtextformel(
+                self.ts.n_words, self.ts.n_polysyllable_words, self.ts.n_monosyllable_words,
+                self.ts.n_long_words, self.ts.n_sents, variant=3),
+            8.432423529411766,
+            places=2)
+
+    def test_wiener_sachtextformel_variant4(self):
+        self.assertAlmostEqual(
+            text_stats.wiener_sachtextformel(
+                self.ts.n_words, self.ts.n_polysyllable_words, self.ts.n_monosyllable_words,
+                self.ts.n_long_words, self.ts.n_sents, variant=4),
+            9.169619607843138,
+            places=2)
--- textacy/text_stats.py
+++ textacy/text_stats.py
@@ -1,38 +1,106 @@
 """
-Calculations for common "readability" statistics.
+Calculations of basic counts and readability statistics for text documents.
 """
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import logging
 from math import sqrt
+import warnings
 
 from spacy.tokens import Doc as SpacyDoc
 
-from textacy import Doc as TextacyDoc
-from textacy import data
-from textacy import extract
+import textacy
+from textacy import data, extract
 
 
 class TextStats(object):
+    """
+    Compute a variety of basic counts and readability statistics for a given
+    text document. For example::
+
+        >>> text = list(textacy.corpora.CapitolWords().texts(limit=1))[0]
+        >>> doc = textacy.Doc(text)
+        >>> ts = TextStats(doc)
+        >>> ts.n_words
+        136
+        >>> ts.flesch_kincaid_grade_level
+        11.817647058823532
+        >>> ts.basic_counts
+        {'n_chars': 685,
+         'n_long_words': 43,
+         'n_monosyllable_words': 90,
+         'n_polysyllable_words': 24,
+         'n_sents': 6,
+         'n_syllables': 214,
+         'n_unique_words': 80,
+         'n_words': 136}
+        >>> ts.readability_stats
+        {'automated_readability_index': 13.626495098039214,
+         'coleman_liau_index': 12.509300816176474,
+         'flesch_kincaid_grade_level': 11.817647058823532,
+         'flesch_readability_ease': 50.707745098039254,
+         'gulpease_index': 51.86764705882353,
+         'gunning_fog_index': 16.12549019607843,
+         'lix': 54.28431372549019,
+         'smog_index': 14.554592549557764,
+         'wiener_sachtextformel': 8.266410784313727}
+
+    Args:
+        doc (:class:`textacy.Doc` or :class:`SpacyDoc`): A text document processed
+            by spacy. Need only be tokenized.
+
+    Attributes:
+        n_sents (int): Number of sentences in ``doc``.
+        n_words (int): Number of words in ``doc``, including numbers + stop words
+            but excluding punctuation.
+        n_chars (int): Number of characters for all words in ``doc``.
+        n_syllables (int): Number of syllables for all words in ``doc``.
+        n_unique_words (int): Number of unique (lower-cased) words in ``doc``.
+        n_long_words (int): Number of words in ``doc`` with 7 or more characters.
+        n_monosyllable_words (int): Number of words in ``doc`` with 1 syllable only.
+        n_polysyllable_words (int): Number of words in ``doc`` with 3 or more syllables.
+            Note: Since this excludes words with exactly 2 syllables, it's likely
+            that ``n_monosyllable_words + n_polysyllable_words != n_words``.
+        flesch_kincaid_grade_level (float): https://en.wikipedia.org/wiki/Flesch%E2%80%93Kincaid_readability_tests#Flesch.E2.80.93Kincaid_grade_level
+        flesch_readability_ease (float): https://en.wikipedia.org/wiki/Flesch%E2%80%93Kincaid_readability_tests#Flesch_reading_ease
+        smog_index (float): https://en.wikipedia.org/wiki/SMOG
+        gunning_fog_index (float): https://en.wikipedia.org/wiki/Gunning_fog_index
+        coleman_liau_index (float): https://en.wikipedia.org/wiki/Coleman%E2%80%93Liau_index
+        automated_readability_index (float): https://en.wikipedia.org/wiki/Automated_readability_index
+        lix (float): https://en.wikipedia.org/wiki/LIX
+        gulpease_index (float): https://it.wikipedia.org/wiki/Indice_Gulpease
+        wiener_sachtextformel (float): https://de.wikipedia.org/wiki/Lesbarkeitsindex#Wiener_Sachtextformel
+            Note: This always returns variant #1.
+        basic_counts (Dict[str, int]): Mapping of basic count names to values,
+            where basic counts are the attributes listed above between ``n_sents``
+            and ``n_polysyllable_words``.
+        readability_stats (Dict[str, float]): Mapping of readability statistic
+            names to values, where readability stats are the attributes listed
+            above between ``flesch_kincaid_grade_level`` and ``wiener_sachtextformel``.
+
+    Raises:
+        ValueError: If ``doc`` is not a :class:`textacy.Doc` or :class:`SpacyDoc`.
+    """
 
     def __init__(self, doc):
         if isinstance(doc, SpacyDoc):
             lang = doc.vocab.lang
             self.n_sents = sum(1 for _ in doc.sents)
-        elif isinstance(doc, TextacyDoc):
+        elif isinstance(doc, textacy.Doc):
             lang = doc.lang
             self.n_sents = doc.n_sents
         else:
             raise ValueError('``doc`` must be a ``textacy.Doc`` or ``spacy.Doc``')
+        # get objs for basic count computations
         hyphenator = data.load_hyphenator(lang=lang)
         words = tuple(extract.words(doc, filter_punct=True, filter_stops=False, filter_nums=False))
         syllables_per_word = tuple(len(hyphenator.positions(word.lower_)) + 1 for word in words)
         chars_per_word = tuple(len(word) for word in words)
-        # compute basic stats needed for most other stats
+        # compute basic counts needed for most readability stats
         self.n_words = len(words)
         self.n_unique_words = len({word.lower for word in words})
-        self.n_long_words = sum(1 for cpw in chars_per_word if cpw >= 7)
         self.n_chars = sum(chars_per_word)
+        self.n_long_words = sum(1 for cpw in chars_per_word if cpw >= 7)
         self.n_syllables = sum(syllables_per_word)
         self.n_monosyllable_words = sum(1 for spw in syllables_per_word if spw == 1)
         self.n_polysyllable_words = sum(1 for spw in syllables_per_word if spw >= 3)
@@ -62,6 +130,10 @@ class TextStats(object):
         return automated_readability_index(self.n_chars, self.n_words, self.n_sents)
 
     @property
+    def lix(self):
+        return lix(self.n_words, self.n_long_words, self.n_sents)
+
+    @property
     def gulpease_index(self):
         return gulpease_index(self.n_chars, self.n_words, self.n_sents)
 
@@ -73,13 +145,13 @@ class TextStats(object):
             variant=1)
 
     @property
-    def basic_stats(self):
+    def basic_counts(self):
         return {'n_sents': self.n_sents,
                 'n_words': self.n_words,
                 'n_chars': self.n_chars,
-                'avg_sent_length': self.n_words / self.n_sents,
-                'avg_word_length': self.n_chars / self.n_words,
+                'n_syllables': self.n_syllables,
                 'n_unique_words': self.n_unique_words,
+                'n_long_words': self.n_long_words,
                 'n_monosyllable_words': self.n_monosyllable_words,
                 'n_polysyllable_words': self.n_polysyllable_words}
 
@@ -91,7 +163,9 @@ class TextStats(object):
                 'gunning_fog_index': self.gunning_fog_index,
                 'coleman_liau_index': self.coleman_liau_index,
                 'automated_readability_index': self.automated_readability_index,
-                'gulpease_index': self.gulpease_index}
+                'lix': self.lix,
+                'gulpease_index': self.gulpease_index,
+                'wiener_sachtextformel': self.wiener_sachtextformel}
 
 
 def readability_stats(doc):
@@ -102,6 +176,8 @@ def readability_stats(doc):
 
     Also includes constituent values needed to compute the stats, e.g. word count.
 
+    **DEPRECATED**
+
     Args:
         doc (:class:`textacy.Doc <textacy.document.Doc>`)
 
@@ -111,6 +187,11 @@ def readability_stats(doc):
     Raises:
         NotImplementedError: if ``doc`` is not English language. sorry.
     """
+    msg = '`readability_stats()` function is deprecated; use `TextStats` class instead'
+    with warnings.catch_warnings():
+        warnings.simplefilter('once', DeprecationWarning)
+        warnings.warn(msg, DeprecationWarning)
+
     if doc.lang != 'en':
         raise NotImplementedError('non-English NLP is not ready yet, sorry')
 
@@ -172,6 +253,11 @@ def automated_readability_index(n_chars, n_words, n_sents):
     return (4.71 * n_chars / n_words) + (0.5 * n_words / n_sents) - 21.43
 
 
+def lix(n_words, n_long_words, n_sents):
+    """https://en.wikipedia.org/wiki/LIX"""
+    return (n_words / n_sents) + (100 * n_long_words / n_words)
+
+
 def wiener_sachtextformel(n_words, n_polysyllable_words, n_monosyllable_words,
                           n_long_words, n_sents,
                           variant=1):
@@ -181,17 +267,17 @@ def wiener_sachtextformel(n_words, n_polysyllable_words, n_monosyllable_words,
     iw = 100 * n_long_words / n_words
     es = 100 * n_monosyllable_words / n_words
     if variant == 1:
-        return 0.1935 * ms + 0.1672 * sl + 0.1297 * iw - 0.0327 * es - 0.875
+        return (0.1935 * ms) + (0.1672 * sl) + (0.1297 * iw) - (0.0327 * es) - 0.875
     elif variant == 2:
-        return 0.2007 * ms + 0.1682 * sl + 0.1373 * iw - 2.779
+        return (0.2007 * ms) + (0.1682 * sl) + (0.1373 * iw) - 2.779
     elif variant == 3:
-        return 0.2963 * ms + 0.1905 * sl - 1.1144
+        return (0.2963 * ms) + (0.1905 * sl) - 1.1144
     elif variant == 4:
-        return 0.2744 * ms + 0.2656 * sl - 1.693
+        return (0.2744 * ms) + (0.2656 * sl) - 1.693
     else:
         raise ValueError('``variant`` value invalid; must be 1, 2, 3, or 4')
 
 
 def gulpease_index(n_chars, n_words, n_sents):
     """https://it.wikipedia.org/wiki/Indice_Gulpease"""
-    return 89 + (300 * n_sents / n_words) - (10 * n_chars / n_words)
+    return (300 * n_sents / n_words) - (10 * n_chars / n_words) + 89
