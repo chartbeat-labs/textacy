ab8413138dfc927f3df523a856d431f39ddf0ae9
Burton DeWilde <burton@chartbeat.com>
2017-11-18 15:25:13 -0600

    Update spacy.load() call for v2; use LRU cache for all objs in data.py

--- tests/test_data.py
+++ tests/test_data.py
@@ -7,8 +7,17 @@ import textacy
 
 class DataTestCase(unittest.TestCase):
 
+    def test_load_spacy(self):
+        for lang in ('en', 'en_core_web_sm'):
+            for disable in (None, ('parser', 'ner')):
+                _ = textacy.data.load_spacy(lang, disable=disable)
+
+    def test_load_spacy_hashability(self):
+        with self.assertRaises(TypeError):
+            _ = textacy.data.load_spacy('en', disable=['tagger', 'parser', 'ner'])
+
     def test_load_pyphen(self):
-        for lang in ('en', 'de'):
+        for lang in ('en', 'de', 'es'):
             _ = textacy.data.load_hyphenator(lang=lang)
 
     def test_load_depechemood(self):
