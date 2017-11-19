79f6bfb310b5aae36bbbe8fd3f9fb75af8c0fa70
Burton DeWilde <burton@chartbeat.com>
2017-11-19 12:20:29 -0600

    Use .text over .orth_ in extract tests

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -21,7 +21,7 @@ class ExtractTestCase(unittest.TestCase):
             'Two', 'weeks', 'ago', ',', 'I', 'was', 'in', 'Kuwait', 'participating',
             'in', 'an', 'I.M.F.', 'seminar', 'for', 'Arab', 'educators', '.', 'For',
             '30', 'minutes', ',', 'we', 'discussed', 'the', 'impact']
-        observed = [tok.orth_ for tok in extract.words(
+        observed = [tok.text for tok in extract.words(
             self.spacy_doc, filter_stops=False, filter_punct=False, filter_nums=False)][:25]
         self.assertEqual(observed, expected)
 
@@ -31,7 +31,7 @@ class ExtractTestCase(unittest.TestCase):
             'educators', 'minutes', 'discussed', 'impact', 'technology', 'trends',
             'education', 'Middle', 'East', 'Egyptian', 'education', 'official',
             'raised', 'hand', 'asked', 'ask', 'personal', 'question']
-        observed = [tok.orth_ for tok in extract.words(
+        observed = [tok.text for tok in extract.words(
             self.spacy_doc, filter_stops=True, filter_punct=True, filter_nums=True)][:25]
         self.assertEqual(observed, expected)
 
@@ -40,7 +40,7 @@ class ExtractTestCase(unittest.TestCase):
             'weeks', 'seminar', 'educators', 'minutes', 'impact', 'technology',
             'trends', 'education', 'education', 'official', 'hand', 'question',
             'mosques', 'sorrow', 'what', 'kids']
-        observed = [tok.orth_ for tok in extract.words(
+        observed = [tok.text for tok in extract.words(
             self.spacy_doc, filter_stops=False, filter_punct=False, filter_nums=False,
             include_pos={'NOUN'})][:25]
         self.assertEqual(observed, expected)
@@ -50,7 +50,7 @@ class ExtractTestCase(unittest.TestCase):
             ',', 'I', 'in', 'in', 'an', 'for', '.', 'For', ',', 'we',
             'the', 'education', 'in', 'the', '.', 'And', 'an',
             'education', 'and', 'he', '"', 'I', 'we', 'to', 'in']
-        observed = [tok.orth_ for tok in extract.words(
+        observed = [tok.text for tok in extract.words(
             self.spacy_doc, filter_stops=False, filter_punct=False, filter_nums=False,
             min_freq=2)][:25]
         self.assertEqual(observed, expected)
@@ -64,7 +64,7 @@ class ExtractTestCase(unittest.TestCase):
             'Two', 'weeks', 'ago', ',', 'I', 'was', 'in', 'Kuwait', 'participating',
             'in', 'an', 'I.M.F.', 'seminar', 'for', 'Arab', 'educators', '.', 'For',
             '30', 'minutes', ',', 'we', 'discussed', 'the', 'impact']
-        observed = [span.orth_ for span in extract.ngrams(
+        observed = [span.text for span in extract.ngrams(
             self.spacy_doc, 1, filter_stops=False, filter_punct=False, filter_nums=False)][:25]
         self.assertEqual(observed, expected)
 
@@ -75,7 +75,7 @@ class ExtractTestCase(unittest.TestCase):
             'seminar for', 'for Arab', 'Arab educators', 'educators.', '. For', 'For 30',
             '30 minutes', 'minutes,', ', we', 'we discussed', 'discussed the', 'the impact',
             'impact of']
-        observed = [span.orth_ for span in extract.ngrams(
+        observed = [span.text for span in extract.ngrams(
             self.spacy_doc, 2, filter_stops=False, filter_punct=False, filter_nums=False)][:25]
         self.assertEqual(observed, expected)
 
@@ -85,20 +85,20 @@ class ExtractTestCase(unittest.TestCase):
             'technology trends', 'Middle East', 'Egyptian education', 'education official',
             'official raised', 'personal question', 'heard Donald', 'Donald Trump',
             'close mosques', 'United States', 'great sorrow']
-        observed = [span.orth_ for span in extract.ngrams(
+        observed = [span.text for span in extract.ngrams(
             self.spacy_doc, 2, filter_stops=True, filter_punct=True, filter_nums=True)]
         self.assertEqual(observed, expected)
 
     def test_ngrams_min_freq(self):
         expected = ['in the', 'in the']
-        observed = [span.orth_ for span in extract.ngrams(
+        observed = [span.text for span in extract.ngrams(
             self.spacy_doc, 2, filter_stops=False, filter_punct=False, filter_nums=False,
             min_freq=2)]
         self.assertEqual(observed, expected)
 
     def test_ngrams_good_tag(self):
         expected = ['technology trends', 'education official']
-        observed = [span.orth_ for span in extract.ngrams(
+        observed = [span.text for span in extract.ngrams(
             self.spacy_doc, 2, filter_stops=False, filter_punct=False, filter_nums=False,
             include_pos={'NOUN'})]
         self.assertEqual(observed, expected)
