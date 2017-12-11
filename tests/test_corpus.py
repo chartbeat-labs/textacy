b6a8061f5eea808268fd0425bbc7478f430fef91
Burton DeWilde <burton@chartbeat.com>
2017-12-11 08:42:37 -0600

    Port unittest-style corpus tests to pytest-style

--- tests/test_corpus.py
+++ tests/test_corpus.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
-import os
-import shutil
-import tempfile
-import unittest
+import pytest
 
 from textacy import Corpus
 from textacy import Doc
@@ -15,24 +12,25 @@ from textacy.datasets.capitol_words import CapitolWords
 
 DATASET = CapitolWords()
 
+pytestmark = pytest.mark.skipif(
+    DATASET.filename is None,
+    reason='CapitolWords dataset must be downloaded before running tests')
 
-@unittest.skipUnless(
-    DATASET.filename, 'CapitolWords dataset must be downloaded before running tests')
-class CorpusInitTestCase(unittest.TestCase):
+
+class TestCorpusInit(object):
 
     def test_corpus_init_lang(self):
-        self.assertIsInstance(Corpus('en'), Corpus)
-        self.assertIsInstance(Corpus(cache.load_spacy('en')), Corpus)
+        assert isinstance(Corpus('en'), Corpus)
+        assert isinstance(Corpus(cache.load_spacy('en')), Corpus)
         for bad_lang in (b'en', None):
-            with self.assertRaises(TypeError):
+            with pytest.raises(TypeError):
                 Corpus(bad_lang)
 
     def test_corpus_init_texts(self):
         limit = 3
         corpus = Corpus('en', texts=DATASET.texts(limit=limit))
-        self.assertEqual(len(corpus.docs), limit)
-        self.assertTrue(
-            all(doc.spacy_vocab is corpus.spacy_vocab for doc in corpus))
+        assert len(corpus.docs) == limit
+        assert all(doc.spacy_vocab is corpus.spacy_vocab for doc in corpus)
 
     def test_corpus_init_texts_and_metadatas(self):
         limit = 3
@@ -41,12 +39,11 @@ class CorpusInitTestCase(unittest.TestCase):
         texts = list(texts)
         metadatas = list(metadatas)
         corpus = Corpus('en', texts=texts, metadatas=metadatas)
-        self.assertEqual(len(corpus.docs), limit)
-        self.assertTrue(
-            all(doc.spacy_vocab is corpus.spacy_vocab for doc in corpus))
+        assert len(corpus.docs) == limit
+        assert all(doc.spacy_vocab is corpus.spacy_vocab for doc in corpus)
         for i in range(limit):
-            self.assertEqual(texts[i], corpus[i].text)
-            self.assertEqual(metadatas[i], corpus[i].metadata)
+            assert texts[i] == corpus[i].text
+            assert metadatas[i] == corpus[i].metadata
 
     def test_corpus_init_docs(self):
         limit = 3
@@ -55,40 +52,32 @@ class CorpusInitTestCase(unittest.TestCase):
         docs = [Doc(text, lang='en', metadata=metadata)
                 for text, metadata in zip(texts, metadatas)]
         corpus = Corpus('en', docs=docs)
-        self.assertEqual(len(corpus.docs), limit)
-        self.assertTrue(
-            all(doc.spacy_vocab is corpus.spacy_vocab for doc in corpus))
+        assert len(corpus.docs) == limit
+        assert all(doc.spacy_vocab is corpus.spacy_vocab for doc in corpus)
         for i in range(limit):
-            self.assertEqual(corpus[i].metadata, docs[i].metadata)
+            assert corpus[i].metadata == docs[i].metadata
         corpus = Corpus(
             'en', docs=docs, metadatas=({'foo': 'bar'} for _ in range(limit)))
         for i in range(limit):
-            self.assertEqual(corpus[i].metadata, {'foo': 'bar'})
+            assert corpus[i].metadata == {'foo': 'bar'}
 
 
-class CorpusMethodsTestCase(unittest.TestCase):
+@pytest.fixture(scope='module')
+def corpus(request):
+    texts, metadatas = fileio.split_record_fields(
+        DATASET.records(limit=3), 'text')
+    corpus = Corpus('en', texts=texts, metadatas=metadatas)
+    return corpus
 
-    def setUp(self):
-        self.tempdir = tempfile.mkdtemp(
-            prefix='test_corpus', dir=os.path.dirname(os.path.abspath(__file__)))
-        texts, metadatas = fileio.split_record_fields(
-            DATASET.records(limit=3), 'text')
-        self.corpus = Corpus('en', texts=texts, metadatas=metadatas)
 
-    def test_corpus_save_and_load(self):
-        filepath = os.path.join(self.tempdir, 'test_corpus_save_and_load.pkl')
-        self.corpus.save(filepath)
-        new_corpus = Corpus.load(filepath)
-        self.assertIsInstance(new_corpus, Corpus)
-        self.assertEqual(len(new_corpus), len(self.corpus))
-        self.assertEqual(new_corpus.lang, self.corpus.lang)
-        self.assertEqual(
-            new_corpus.spacy_lang.pipe_names,
-            self.corpus.spacy_lang.pipe_names)
-        self.assertIsNone(
-            new_corpus[0].spacy_doc.user_data['textacy'].get('spacy_lang_meta'))
-        for i in range(len(new_corpus)):
-            self.assertEqual(new_corpus[i].metadata, self.corpus[i].metadata)
-
-    def tearDown(self):
-        shutil.rmtree(self.tempdir)
+def test_corpus_save_and_load(tmpdir, corpus):
+    filepath = str(tmpdir.join('test_corpus_save_and_load.pkl'))
+    corpus.save(filepath)
+    new_corpus = Corpus.load(filepath)
+    assert isinstance(new_corpus, Corpus)
+    assert len(new_corpus) == len(corpus)
+    assert new_corpus.lang == corpus.lang
+    assert new_corpus.spacy_lang.pipe_names == corpus.spacy_lang.pipe_names
+    assert new_corpus[0].spacy_doc.user_data['textacy'].get('spacy_lang_meta') is None
+    for i in range(len(new_corpus)):
+        assert new_corpus[i].metadata == corpus[i].metadata
