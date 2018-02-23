ec16774d32aa86f3bc42dd9930ef758abde9a002
Burton DeWilde <burton@chartbeat.com>
2018-02-22 23:30:35 -0500

    Fix broken topic model test from modified vectorizer init params

--- tests/test_topic_model.py
+++ tests/test_topic_model.py
@@ -27,7 +27,7 @@ def term_lists():
 @pytest.fixture(scope='module')
 def vectorizer():
     vectorizer_ = Vectorizer(
-        weighting='tf', normalize=False, sublinear_tf=False, smooth_idf=True,
+        weighting='tf', norm=None, tf_scale=None, idf_type='smooth',
         min_df=1, max_df=1.0, min_ic=0.0, max_n_terms=None)
     return vectorizer_
 
