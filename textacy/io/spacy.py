6a44f3a4de32544f34d95e66774b016ab6e8d04a
Burton DeWilde <burtdewilde@gmail.com>
2019-03-22 13:37:45 -0500

    Fix bug in experimental read spacy docs func; closes #228
    Credit where it's due — this is just a re-implementation of https://github.com/chartbeat-labs/textacy/pull/229
    
    Also catch a mystery exception for unicodedecodeerror that I have to track down

--- textacy/io/spacy.py
+++ textacy/io/spacy.py
@@ -83,11 +83,11 @@ def read_spacy_docs(fname, format="pickle", lang=None):
                 # vexing for user data. As a best guess, we *know* that within
                 # keys, we must have tuples. In values we just have to hope
                 # users don't mind getting a list instead of a tuple.
-                if "user_data_keys" in msg:
+                if b"user_data_keys" in msg:
                     user_data_keys = msgpack.loads(
-                        msg["user_data_keys"], use_list=False
+                        msg[b"user_data_keys"], use_list=False
                     )
-                    user_data_values = msgpack.loads(msg["user_data_values"])
+                    user_data_values = msgpack.loads(msg[b"user_data_values"])
                     user_data = {
                         key: value
                         for key, value in compat.zip_(user_data_keys, user_data_values)
@@ -95,26 +95,29 @@ def read_spacy_docs(fname, format="pickle", lang=None):
                 else:
                     user_data = None
 
-                text = msg["text"]
-                attrs = msg["array_body"]
+                text = msg[b"text"]
+                attrs = msg[b"array_body"]
                 words = []
                 spaces = []
                 start = 0
                 for i in compat.range_(attrs.shape[0]):
                     end = start + int(attrs[i, 0])
                     has_space = int(attrs[i, 1])
-                    words.append(text[start:end])
+                    try:
+                        words.append(compat.bytes_to_unicode(text[start: end]))
+                    except Exception as e:
+                        continue  # FIXME
                     spaces.append(bool(has_space))
                     start = end + has_space
 
                 spacy_doc = SpacyDoc(
                     vocab, words=words, spaces=spaces, user_data=user_data
                 )
-                spacy_doc = spacy_doc.from_array(msg["array_head"][2:], attrs[:, 2:])
-                if "sentiment" in msg:
-                    spacy_doc.sentiment = msg["sentiment"]
-                if "tensor" in msg:
-                    spacy_doc.tensor = msg["tensor"]
+                spacy_doc = spacy_doc.from_array(msg[b"array_head"][2:], attrs[:, 2:])
+                if b"sentiment" in msg:
+                    spacy_doc.sentiment = msg[b"sentiment"]
+                if b"tensor" in msg:
+                    spacy_doc.tensor = msg[b"tensor"]
                 yield spacy_doc
     else:
         raise ValueError(
