c597474421b9743e438340bc0b4704f1708e6469
Burton DeWilde <burtdewilde@gmail.com>
2019-05-09 15:54:52 -0500

    Update and reorg corpus tests for new API
    TODO: document metadata tests!

--- tests/test_corpus.py
+++ tests/test_corpus.py
@@ -3,6 +3,8 @@ from __future__ import absolute_import, unicode_literals
 
 import pytest
 
+import spacy
+
 from textacy import Corpus
 from textacy import Doc
 from textacy import cache
@@ -20,75 +22,61 @@ pytestmark = pytest.mark.skipif(
 
 @pytest.fixture(scope="module")
 def corpus(request):
-    texts, metas = io.unzip(DATASET.records(limit=3))
-    corpus = Corpus("en", texts=texts, metadatas=metas)
-    return corpus
+    return Corpus("en", data=DATASET.records(limit=3))
 
 
-# init tests
+class TestCorpusInit(object):
 
-def test_corpus_init_lang():
-    assert isinstance(Corpus("en"), Corpus)
-    assert isinstance(Corpus(cache.load_spacy("en")), Corpus)
-    for bad_lang in (b"en", None):
-        with pytest.raises(TypeError):
-            Corpus(bad_lang)
+    def test_corpus_init_lang(self):
+        assert isinstance(Corpus("en"), Corpus)
+        assert isinstance(Corpus(cache.load_spacy("en")), Corpus)
+        for bad_lang in (b"en", None):
+            with pytest.raises(TypeError):
+                Corpus(bad_lang)
 
+    def test_corpus_init_texts(self):
+        limit = 3
+        texts = list(DATASET.texts(limit=limit))
+        corpus = Corpus("en", data=texts)
+        assert len(corpus) == len(corpus.docs) == limit
+        assert all(doc.vocab is corpus.spacy_lang.vocab for doc in corpus)
+        assert all(text == doc.text for text, doc in zip(texts, corpus))
 
-def test_corpus_init_texts():
-    limit = 3
-    corpus = Corpus("en", texts=DATASET.texts(limit=limit))
-    assert len(corpus.docs) == limit
-    assert all(doc.spacy_vocab is corpus.spacy_vocab for doc in corpus)
+    def test_corpus_init_records(self):
+        limit = 3
+        records = list(DATASET.records(limit=limit))
+        corpus = Corpus("en", data=records)
+        assert len(corpus) == len(corpus.docs) == limit
+        assert all(doc.vocab is corpus.spacy_lang.vocab for doc in corpus)
+        assert all(record[0] == doc.text for record, doc in zip(records, corpus))
+        # TODO: handle document metadata!
+        # assert all(record[1] == doc._.meta for record, doc in zip(records, corpus))
 
+    def test_corpus_init_docs(self):
+        limit = 3
+        spacy_lang = cache.load_spacy("en")
+        texts = DATASET.texts(limit=limit)
+        docs = [spacy_lang(text) for text in texts]
+        corpus = Corpus("en", data=docs)
+        assert len(corpus) == len(corpus.docs) == limit
+        assert all(doc.vocab is corpus.spacy_lang.vocab for doc in corpus)
+        assert all(doc1 is doc2 for doc1, doc2 in zip(docs, corpus))
 
-def test_corpus_init_texts_and_metadatas():
-    limit = 3
-    texts, metas = io.unzip(DATASET.records(limit=limit))
-    texts = list(texts)
-    metas = list(metas)
-    corpus = Corpus("en", texts=texts, metadatas=metas)
-    assert len(corpus.docs) == limit
-    assert all(doc.spacy_vocab is corpus.spacy_vocab for doc in corpus)
-    for i in range(limit):
-        assert texts[i] == corpus[i].text
-        assert metas[i] == corpus[i].metadata
-
-
-def test_corpus_init_docs():
-    limit = 3
-    texts, metas = io.unzip(DATASET.records(limit=limit))
-    docs = [
-        Doc(text, lang="en", metadata=meta)
-        for text, meta in zip(texts, metas)
-    ]
-    corpus = Corpus("en", docs=docs)
-    assert len(corpus.docs) == limit
-    assert all(doc.spacy_vocab is corpus.spacy_vocab for doc in corpus)
-    for i in range(limit):
-        assert corpus[i].metadata == docs[i].metadata
-    corpus = Corpus("en", docs=docs, metadatas=({"foo": "bar"} for _ in range(limit)))
-    for i in range(limit):
-        assert corpus[i].metadata == {"foo": "bar"}
-
-
-def test_corpus_init_no_parser():
-    spacy_lang = cache.load_spacy("en", disable=("parser",))
-    corpus = Corpus(spacy_lang, docs=(spacy_lang("This is a sentence in a doc."),))
-    assert corpus.n_sents is None and len(corpus) == 1
-
-
-# methods tests
+    def test_corpus_init_no_parser(self):
+        spacy_lang = cache.load_spacy("en", disable=("parser",))
+        corpus = Corpus(spacy_lang, data=(spacy_lang("This is a sentence in a doc."),))
+        assert len(corpus) == 1
+        assert corpus.n_sents == 0
 
 
 def test_corpus_save_and_load(tmpdir, corpus):
     filepath = str(tmpdir.join("test_corpus_save_and_load.pkl"))
     corpus.save(filepath)
-    new_corpus = Corpus.load(filepath)
-    assert isinstance(new_corpus, Corpus)
-    assert len(new_corpus) == len(corpus)
-    assert new_corpus.lang == corpus.lang
-    assert new_corpus.spacy_lang.pipe_names == corpus.spacy_lang.pipe_names
-    assert new_corpus[0].spacy_doc.user_data["textacy"].get("spacy_lang_meta") is None
-    for i in range(len(new_corpus)):
-        assert new_corpus[i].metadata == corpus[i].metadata
+    loaded_corpus = Corpus.load("en", filepath)
+    assert isinstance(loaded_corpus, Corpus)
+    assert len(loaded_corpus) == len(corpus)
+    assert loaded_corpus.spacy_lang.meta == corpus.spacy_lang.meta
+    assert loaded_corpus.spacy_lang.pipe_names == corpus.spacy_lang.pipe_names
+
+
+# TODO: add more tests :)
