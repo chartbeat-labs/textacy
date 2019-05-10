580c7a473d92e0df6c17e45ed4d3ac9fdecebe82
Burton DeWilde <burtdewilde@gmail.com>
2019-05-09 23:29:13 -0500

    Replace textacy.Doc with spacy doc extensions
    - add functions to set and remove all doc extensions, and set them on textacy init
    - add convenience function to easily make a spacy doc, akin to what was done for making a textacy.Doc and the new textacy.Corpus

--- textacy/__init__.py
+++ textacy/__init__.py
@@ -8,26 +8,15 @@ from .about import __version__
 
 data_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
 
-# subpackages
-# from textacy import io
-# from textacy import tm
-# from textacy import viz
-# top-level modules
-# from textacy import compat, constants, math_utils, spacy_pipelines, vsm
-# from textacy import data, preprocess, text_utils
-# from textacy import lexicon_methods, spacy_utils, text_stats
-# from textacy import doc
-# from textacy import corpus
-# from textacy import extract
-# from textacy import export, keyterms, network, similarity
-
 from textacy.cache import load_spacy
 from textacy.preprocess import preprocess_text
-from textacy.doc import Doc
+from textacy.doc import make_spacy_doc, remove_doc_extensions, set_doc_extensions
 from textacy.corpus import Corpus
 from textacy.text_stats import TextStats
 from textacy.tm import TopicModel
 from textacy.vsm import Vectorizer
+
+set_doc_extensions()
 
 logger = logging.getLogger("textacy")
 if len(logger.handlers) == 0:  # ensure reload() doesn't add another handler
