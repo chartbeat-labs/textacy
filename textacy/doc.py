9db400554b8b92b45f9b74ebd322c1ac21a7868f
Burton DeWilde <burtdewilde@gmail.com>
2019-05-10 16:11:05 -0500

    Remove doc extensions from doc module and tests

--- textacy/doc.py
+++ textacy/doc.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# TODO: module docstring
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import types
@@ -10,44 +9,6 @@ from . import cache
 from . import compat
 from . import text_utils
 from . import utils
-from .spacier.doc_extensions import *
-
-
-_doc_extensions = {
-    # property extensions
-    "lang": {"getter": get_lang},
-    "preview": {"getter": get_preview},
-    "tokens": {"getter": get_tokens},
-    "meta": {"getter": get_meta, "setter": set_meta},
-    "n_tokens": {"getter": get_n_tokens},
-    "n_sents": {"getter": get_n_sents},
-    # method extensions
-    "to_tokenized_text": {"method": to_tokenized_text},
-    "to_tagged_text": {"method": to_tagged_text},
-    "to_terms_list": {"method": to_terms_list},
-    "to_bag_of_terms": {"method": to_bag_of_terms},
-    "to_bag_of_words": {"method": to_bag_of_words},
-    "to_semantic_network": {"method": to_semantic_network},
-}
-
-
-def set_doc_extensions():
-    """
-    Set textacy's custom property and method doc extensions
-    on the global :class:`spacy.tokens.Doc`.
-    """
-    for name, kwargs in _doc_extensions.items():
-        if not spacy.tokens.Doc.has_extension(name):
-            spacy.tokens.Doc.set_extension(name, **kwargs)
-
-
-def remove_doc_extensions():
-    """
-    Remove textacy's custom property and method doc extensions
-    from the global :class:`spacy.tokens.Doc`.
-    """
-    for name in _doc_extensions.keys():
-        _ = spacy.tokens.Doc.remove_extension(name)
 
 
 def make_spacy_doc(data, lang=text_utils.detect_language):
