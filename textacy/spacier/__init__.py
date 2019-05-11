105c2b75a8a12ba9c11c6d085113e2c2fcd75688
Burton DeWilde <burtdewilde@gmail.com>
2019-05-11 11:12:57 -0500

    Back those spacier imports right back out
    for PY2's sake

--- textacy/spacier/__init__.py
+++ textacy/spacier/__init__.py
@@ -1,3 +1,5 @@
-from . import components
-from . import doc_extensions
-from . import utils
+# PY2 has a problem with these imports, so let's just punt until PY2 retires
+# and we no longer have to support it :)
+# from . import components
+# from . import doc_extensions
+# from . import utils
