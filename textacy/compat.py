e5e61987a2ebc913863738c6a9fb533bc7d10b7d
Burton DeWilde <burtdewilde@gmail.com>
2019-05-11 12:05:23 -0500

    Remove deprecated functionality from compat

--- textacy/compat.py
+++ textacy/compat.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 import sys
 
 PY2 = sys.version_info.major == 2
-is_python2 = int(sys.version[0]) == 2  # TODO: deprecate this
 is_windows = sys.platform.startswith("win")
 is_linux = sys.platform.startswith("linux")
 is_osx = sys.platform == "darwin"
@@ -23,10 +22,6 @@ if PY2:
     int_types = (int, long)
     chr_ = unichr
 
-    # TODO: deprecate this
-    def bytes_to_unicode(b, encoding="utf8", errors="strict"):
-        return unicode_(b, encoding=encoding, errors=errors)
-
 else:
     import csv
     import pickle
@@ -41,15 +36,6 @@ else:
     string_types = (bytes, str)
     int_types = (int,)
     chr_ = chr
-
-    # TODO: deprecate this
-    def bytes_to_unicode(b, encoding="utf8", errors="strict"):
-        return b.decode(encoding=encoding, errors=errors)
-
-
-# TODO: deprecate this
-def unicode_to_bytes(s, encoding="utf8", errors="strict"):
-    return s.encode(encoding=encoding, errors=errors)
 
 
 def to_bytes(s, encoding="utf-8", errors="strict"):
