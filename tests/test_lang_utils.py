6008924ba145fe3604adad3cad6e9137dcaae559
Burton DeWilde <burtdewilde@gmail.com>
2019-06-03 17:59:36 -0500

    Add tests for lang identifier

--- tests/test_lang_utils.py
+++ tests/test_lang_utils.py
@@ -3,16 +3,34 @@ from __future__ import absolute_import, unicode_literals
 
 from textacy import lang_utils
 
-LANG_SENTS = [
+GOOD_LANG_SENTS = [
     ("en", "This sentence is in English."),
     ("es", "Esta oración es en Español."),
     ("fr", "Cette phrase est en français."),
+]
+
+BAD_LANG_SENTS = [
     ("un", "1"),
     ("un", " "),
     ("un", ""),
 ]
 
 
-def test_detect_language():
-    for lang, sent in LANG_SENTS:
-        assert lang_utils.detect_lang(sent) == lang
+def test_identify_lang():
+    for lang, sent in GOOD_LANG_SENTS + BAD_LANG_SENTS:
+        assert lang_utils.identify_lang(sent) == lang
+
+
+def test_identify_topn_langs():
+    for lang, sent in GOOD_LANG_SENTS + BAD_LANG_SENTS:
+        topn_langs = lang_utils.lang_identifier.identify_topn_langs(sent)
+        assert topn_langs[0][0] == lang
+        assert topn_langs[0][1] >= 0.5
+
+
+def test_identify_topn_langs_topn():
+    for n in [1, 2, 3]:
+        topn_langs = lang_utils.lang_identifier.identify_topn_langs(
+            GOOD_LANG_SENTS[0][1], topn=n)
+        assert isinstance(topn_langs, list)
+        assert len(topn_langs) == n
