fa40c398d7309414f87879190d04e3f9f81e2dcb
Burton DeWilde <burtdewilde@gmail.com>
2019-06-26 18:49:11 -0500

    Import all preproc funcs at top level of subpkg

---
+++ textacy/preprocessing/__init__.py
@@ -0,0 +1,14 @@
+from .normalize import (
+    normalize_contractions,
+    normalize_hyphenated_words,
+    normalize_unicode,
+    normalize_whitespace,
+)
+from .remove import remove_accents, remove_punctuation
+from .replace import (
+    replace_currency_symbols,
+    replace_emails,
+    replace_numbers,
+    replace_phone_numbers,
+    replace_urls,
+)
