252b67807632d4ce78d1fd02bdccb81ed0b837b4
Burton DeWilde <burtdewilde@gmail.com>
2019-06-27 12:52:11 -0500

    Add funcs + tests to replace hashtags, user handles

--- tests/preprocessing/test_replace.py
+++ tests/preprocessing/test_replace.py
@@ -25,6 +25,17 @@ def test_replace_emails():
         assert preprocessing.replace_emails(in_) == out_
 
 
+def test_replace_hashtags():
+    in_outs = [
+        ("like omg it's #ThrowbackThursday", "like omg it's _TAG_"),
+        ("#TextacyIn4Words: \"but it's honest work\"", "_TAG_: \"but it's honest work\""),
+        ("wth twitter #ican'teven #why-even-try", "wth twitter _TAG_'teven _TAG_-even-try"),
+        ("www.foo.com#fragment is not a hashtag", "www.foo.com#fragment is not a hashtag"),
+    ]
+    for in_, out_ in in_outs:
+        assert preprocessing.replace_hashtags(in_) == out_
+
+
 def test_replace_numbers():
     text = "I owe $1,000.99 to 123 people for 2 +1 reasons."
     proc_text = "I owe $_NUM_ to _NUM_ people for _NUM_ _NUM_ reasons."
@@ -41,3 +52,14 @@ def test_replace_urls():
     text = "I learned everything I know from www.stackoverflow.com and http://wikipedia.org/ and Mom."
     proc_text = "I learned everything I know from _URL_ and _URL_ and Mom."
     assert preprocessing.replace_urls(text, "_URL_") == proc_text
+
+
+def test_replace_user_handles():
+    in_outs = [
+        ("like omg it's @bjdewilde", "like omg it's _USER_"),
+        ("@Real_Burton_DeWilde: definitely not a bot", "_USER_: definitely not a bot"),
+        ("wth twitter @b.j.dewilde", "wth twitter _USER_.j.dewilde"),
+        ("foo@bar.com is not a user handle", "foo@bar.com is not a user handle"),
+    ]
+    for in_, out_ in in_outs:
+        assert preprocessing.replace_user_handles(in_) == out_
--- textacy/preprocessing/replace.py
+++ textacy/preprocessing/replace.py
@@ -3,10 +3,12 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from .resources import (
     RE_CURRENCY_SYMBOL,
     RE_EMAIL,
+    RE_HASHTAG,
     RE_NUMBER,
     RE_PHONE_NUMBER,
     RE_SHORT_URL,
     RE_URL,
+    RE_USER_HANDLE,
 )
 
 
@@ -52,6 +54,20 @@ def replace_numbers(text, replace_with="_NUMBER_"):
     return RE_NUMBER.sub(replace_with, text)
 
 
+def replace_hashtags(text, replace_with="_TAG_"):
+    """
+    Replace all hashtags in ``text`` with ``replace_with``.
+
+    Args:
+        text (str)
+        replace_with (str)
+
+    Returns:
+        str
+    """
+    return RE_HASHTAG.sub(replace_with, text)
+
+
 def replace_phone_numbers(text, replace_with="_PHONE_"):
     """
     Replace all phone numbers in ``text`` with ``replace_with``.
@@ -78,3 +94,17 @@ def replace_urls(text, replace_with="_URL_"):
         str
     """
     return RE_URL.sub(replace_with, RE_SHORT_URL.sub(replace_with, text))
+
+
+def replace_user_handles(text, replace_with="_USER_"):
+    """
+    Replace all user handles in ``text`` with ``replace_with``.
+
+    Args:
+        text (str)
+        replace_with (str)
+
+    Returns:
+        str
+    """
+    return RE_USER_HANDLE.sub(replace_with, text)
