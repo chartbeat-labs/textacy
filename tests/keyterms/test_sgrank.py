f5737db69556bbc3ed422281bcc7d98ef8fcec3b
Burton DeWilde <burtdewilde@gmail.com>
2019-07-05 13:38:19 -0500

    Test the new sgrank, not the old one

--- tests/keyterms/test_sgrank.py
+++ tests/keyterms/test_sgrank.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import pytest
 
 import textacy
-from textacy import compat, datasets, keyterms
+from textacy import compat, datasets, ke
 
 
 @pytest.fixture(scope="module")
@@ -19,7 +19,7 @@ def empty_spacy_doc():
 
 
 def test_default(spacy_doc):
-    result = keyterms.sgrank(spacy_doc)
+    result = ke.sgrank(spacy_doc)
     assert isinstance(result, list) and len(result) > 0
     assert all(isinstance(ts, tuple) and len(ts) == 2 for ts in result)
     assert all(
@@ -29,58 +29,58 @@ def test_default(spacy_doc):
 
 
 def test_ngrams_1(spacy_doc):
-    result = keyterms.sgrank(spacy_doc, ngrams=1)
+    result = ke.sgrank(spacy_doc, ngrams=1)
     assert len(result) > 0
     assert all(len(term.split()) == 1 for term, _ in result)
 
 
 def test_ngrams_2_3(spacy_doc):
-    result = keyterms.sgrank(spacy_doc, ngrams=(2, 3))
+    result = ke.sgrank(spacy_doc, ngrams=(2, 3))
     assert len(result) > 0
     assert all(2 <= len(term.split()) <= 3 for term, _ in result)
 
 
 def test_n_keyterms(spacy_doc):
     for n in (5, 25):
-        result = keyterms.sgrank(spacy_doc, n_keyterms=n)
+        result = ke.sgrank(spacy_doc, n_keyterms=n)
         assert 0 < len(result) <= n
 
 
 def test_n_keyterms_float(spacy_doc):
-    result = keyterms.sgrank(spacy_doc, n_keyterms=0.2)
+    result = ke.sgrank(spacy_doc, n_keyterms=0.2)
     assert len(result) > 0
     with pytest.raises(ValueError):
-        _ = keyterms.sgrank(spacy_doc, n_keyterms=2.0)
+        _ = ke.sgrank(spacy_doc, n_keyterms=2.0)
 
 
 def test_normalize_lower(spacy_doc):
-    result = keyterms.sgrank(spacy_doc, normalize="lower")
+    result = ke.sgrank(spacy_doc, normalize="lower")
     assert len(result) > 0
     assert all(term == term.lower() for term, _ in result)
 
 
 def test_normalize_none(spacy_doc):
-    result = keyterms.sgrank(spacy_doc, normalize=None)
+    result = ke.sgrank(spacy_doc, normalize=None)
     assert len(result) > 0
     assert any(term != term.lower() for term, _ in result)
 
 
 def test_normalize_callable(spacy_doc):
-    result = keyterms.sgrank(spacy_doc, normalize=lambda tok: tok.upper_)
+    result = ke.sgrank(spacy_doc, normalize=lambda tok: tok.upper_)
     assert len(result) > 0
     assert all(term == term.upper() for term, _ in result)
 
 
 def test_window_width(spacy_doc):
-    result_10 = keyterms.sgrank(spacy_doc, window_width=10)
-    result_100 = keyterms.sgrank(spacy_doc, window_width=100)
+    result_10 = ke.sgrank(spacy_doc, window_width=10)
+    result_100 = ke.sgrank(spacy_doc, window_width=100)
     assert len(result_10) > 0 and len(result_100) > 0
     assert result_10 != result_100
     with pytest.raises(ValueError):
-        _ = keyterms.sgrank(spacy_doc, window_width=1)
+        _ = ke.sgrank(spacy_doc, window_width=1)
 
 
 def test_empty_doc(empty_spacy_doc):
-    result = keyterms.sgrank(empty_spacy_doc)
+    result = ke.sgrank(empty_spacy_doc)
     assert isinstance(result, list)
     assert len(result) == 0
