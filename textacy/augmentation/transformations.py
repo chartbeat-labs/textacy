eaaf2bad32af4bda1a98d434530926bb6a001230
Burton DeWilde <burtdewilde@gmail.com>
2019-08-23 18:04:55 -0500

    Improve transforms handling for ws and null syns

--- textacy/augmentation/transformations.py
+++ textacy/augmentation/transformations.py
@@ -1,6 +1,8 @@
 import collections
 import random
 
+from cytoolz import dicttoolz
+
 from .. import resources
 from ..doc import make_spacy_doc
 
@@ -49,6 +51,8 @@ def augment(
             for item in sent_items
             if item.is_word
         }
+        # only keep items with non-empty synonym lists
+        synonyms = dicttoolz.valfilter(lambda v: v, synonyms)
         sent_items = replace_with_synonyms(sent_items, synonyms, n_replacements)
         sent_items = insert_synonyms(sent_items, synonyms, n_insertions)
         sent_items = swap_items(sent_items, n_swaps)
@@ -73,24 +77,24 @@ def replace_with_synonyms(items, synonyms, n):
     Returns:
         List[Item]
     """
-    replacements = random.sample(
-        [
-            (idx, synonyms[(item.text, item.pos)])
-            for idx, item in enumerate(items)
-            if item.is_word
-        ],
-        n,
-    )
-    for idx, syns in replacements:
-        if syns:
-            curr_item = items[idx]
-            items[idx] = Item(
-                tok=None,
-                text=random.choice(syns),
-                ws=curr_item.ws,
-                pos=curr_item.pos,
-                is_word=True,
-            )
+    candidate_idx_syns = [
+        (idx, synonyms[(item.text, item.pos)])
+        for idx, item in enumerate(items)
+        if item.is_word and (item.text, item.pos) in synonyms
+    ]
+    if not candidate_idx_syns:
+        return items
+
+    random_idx_syns = random.sample(candidate_idx_syns, min(n, len(candidate_idx_syns)))
+    for idx, syns in random_idx_syns:
+        curr_item = items[idx]
+        items[idx] = Item(
+            tok=None,
+            text=random.choice(syns),
+            ws=curr_item.ws,
+            pos=curr_item.pos,
+            is_word=True,
+        )
     return items
 
 
@@ -104,15 +108,14 @@ def insert_synonyms(items, synonyms, n):
     Returns:
         List[Item]
     """
-    for _ in range(n):
-        text_pos, syns = random.choice(list(synonyms.items()))
-        # FIXME: we should skip empty syns, if others are available...
-        if not syns:
-            continue
-        pos = text_pos[1]
+    random_synonyms = random.sample(synonyms.items(), min(n, len(synonyms)))
+    if not random_synonyms or len(items) < 3:
+        return items
+
+    for (_, pos), syns in random_synonyms:
         random_syn = random.choice(syns)
         random_idx = random.randint(0, len(items) - 1)
-        ws = items[random_idx].ws
+        ws = items[random_idx - 1].ws
         # insert synonym into the list
         items.insert(
             random_idx,
@@ -148,9 +151,25 @@ def swap_items(items, n):
         for pos in swap_pos:
             idxs = [idx for idx, item in enumerate(items) if item.pos == pos]
             if len(idxs) >= 2:
-                # FIXME: whitespace should not be swapped :/
                 idx1, idx2 = random.sample(idxs, 2)
-                items[idx2], items[idx1] = items[idx1], items[idx2]
+                # can't do this, bc whitespace should not be swapped :/
+                # items[idx2], items[idx1] = items[idx1], items[idx2]
+                item1 = items[idx1]
+                item2 = items[idx2]
+                items[idx1] = Item(
+                    tok=item2.tok,
+                    text=item2.text,
+                    ws=item1.ws,
+                    pos=item2.pos,
+                    is_word=item2.is_word,
+                )
+                items[idx2] = Item(
+                    tok=item1.tok,
+                    text=item1.text,
+                    ws=item2.ws,
+                    pos=item1.pos,
+                    is_word=item1.is_word,
+                )
                 break
     return items
 
@@ -168,5 +187,17 @@ def delete_items(items, prob):
         idx for idx, item in enumerate(items) if random.random() < prob and item.is_word
     ]
     for idx in sorted(delete_idxs, reverse=True):
+        # TODO: determine if we want to adjust whitespace here
+        # if idx > 0:
+        #     prev_item = items[idx - 1]
+        #     curr_item = items[idx]
+        #     if prev_item.ws != curr_item.ws:
+        #         items[idx - 1] = Item(
+        #             tok=prev_item.tok,
+        #             text=prev_item.text,
+        #             ws=curr_item.ws,
+        #             pos=prev_item.pos,
+        #             is_word=prev_item.is_word,
+        #         )
         del items[idx]
     return items
