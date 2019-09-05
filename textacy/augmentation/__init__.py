913d61bc2df0d634c0beb46e823eeebe898954e3
Burton DeWilde <burtdewilde@gmail.com>
2019-09-05 08:45:11 -0500

    Add top-level imports to augmentation subpkg

--- textacy/augmentation/__init__.py
+++ textacy/augmentation/__init__.py
@@ -1,1 +1,12 @@
 from .augmenter import Augmenter
+from .transforms import (
+    substitute_word_synonyms,
+    insert_word_synonyms,
+    swap_words,
+    delete_words,
+    substitute_chars,
+    insert_chars,
+    swap_chars,
+    delete_chars,
+)
+from .utils import to_aug_toks, get_char_weights
