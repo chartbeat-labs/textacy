ec57bd3a0d4d13dbc6f13063a9fe1c637bd685ef
Burton DeWilde <burtdewilde@gmail.com>
2020-02-05 22:05:27 -0600

    Add type annotations to similarity.py

--- textacy/similarity.py
+++ textacy/similarity.py
@@ -8,13 +8,16 @@ and 1.0 (totally similar).
 """
 import collections
 import re
+from typing import Dict, Sequence, Union
 
 import numpy as np
 import sklearn.feature_extraction
+import spacy
 from cytoolz import itertoolz
 from jellyfish import levenshtein_distance as _levenshtein
 from pyemd import emd
 from sklearn.metrics import pairwise_distances
+from spacy.tokens import Doc, Span, Token
 
 from . import extract
 
@@ -22,18 +25,17 @@ from . import extract
 RE_ALNUM = re.compile(r"[^\W_]+", flags=re.IGNORECASE | re.UNICODE)
 
 
-def word_movers(doc1, doc2, metric="cosine"):
+def word_movers(doc1: Doc, doc2: Doc, metric: str = "cosine") -> float:
     """
-    Measure the semantic similarity between two documents using Word Movers
-    Distance.
+    Measure the semantic similarity between two documents using Word Movers Distance.
 
     Args:
-        doc1 (:class:`spacy.tokens.Doc`)
-        doc2 (:class:`spacy.tokens.Doc`)
+        doc1
+        doc2
         metric ({"cosine", "euclidean", "l1", "l2", "manhattan"})
 
     Returns:
-        float: Similarity between ``doc1`` and ``doc2`` in the interval [0.0, 1.0],
+        Similarity between ``doc1`` and ``doc2`` in the interval [0.0, 1.0],
         where larger values correspond to more similar documents.
 
     References:
@@ -45,7 +47,7 @@ def word_movers(doc1, doc2, metric="cosine"):
           Proceedings of the 32nd International Conference on Machine Learning
           (ICML 2015). 2015. http://jmlr.org/proceedings/papers/v37/kusnerb15.pdf
     """
-    word_idxs = dict()
+    word_idxs: Dict[str, int] = dict()
 
     n = 0
     word_vecs = []
@@ -77,42 +79,46 @@ def word_movers(doc1, doc2, metric="cosine"):
     return 1.0 - emd(vec1, vec2, distance_mat)
 
 
-def word2vec(obj1, obj2):
+def word2vec(obj1: Union[Doc, Span, Token], obj2 : Union[Doc, Span, Token]) -> float:
     """
-    Measure the semantic similarity between one spacy Doc, Span, Token,
-    or Lexeme and another like object using the cosine distance between the
-    objects' (average) word2vec vectors.
+    Measure the semantic similarity between one spacy Doc, Span, Token, or Lexeme
+    and another like object using the cosine distance between the objects' (average)
+    word2vec vectors.
 
     Args:
-        obj1 (:class:`spacy.tokens.Doc`, :class:`spacy.tokens.Span`, :class:`spacy.tokens.Token`, or ``spacy.Lexeme``)
-        obj2 (:class:`spacy.tokens.Doc`, :class:`spacy.tokens.Span`, :class:`spacy.tokens.Token`, or ``spacy.Lexeme``)
+        obj1
+        obj2
 
     Returns
-        float: Similarity between ``obj1`` and ``obj2`` in the interval [0.0, 1.0],
+        Similarity between ``obj1`` and ``obj2`` in the interval [0.0, 1.0],
         where larger values correspond to more similar objects
     """
     return obj1.similarity(obj2)
 
 
-def jaccard(obj1, obj2, fuzzy_match=False, match_threshold=0.8):
+def jaccard(
+    obj1: Union[str, Sequence[str]],
+    obj2: Union[str, Sequence[str]],
+    fuzzy_match: bool = False,
+    match_threshold: float = 0.8,
+) -> float:
     """
     Measure the similarity between two strings or sequences of strings using
     Jaccard distance, with optional fuzzy matching of not-identical pairs
     when ``obj1`` and ``obj2`` are sequences of strings.
 
     Args:
-        obj1 (str or Sequence[str])
-        obj2 (str or Sequence[str]): if str, both inputs are treated as sequences
-            of *characters*, in which case fuzzy matching is not permitted
-        fuzzy_match (bool): if True, allow for fuzzy matching in addition to the
+        obj1
+        obj2: If str, both inputs are treated as sequences of *characters*,
+            in which case fuzzy matching is not permitted
+        fuzzy_match: If True, allow for fuzzy matching in addition to the
             usual identical matching of pairs between input vectors
-        match_threshold (float): value in the interval [0.0, 1.0]; fuzzy comparisons
+        match_threshold: Value in the interval [0.0, 1.0]; fuzzy comparisons
             with a score >= this value will be considered matches
 
     Returns:
-        float: Similarity between ``obj1`` and ``obj2`` in the interval [0.0, 1.0],
-        where larger values correspond to more similar strings or sequences
-        of strings
+        Similarity between ``obj1`` and ``obj2`` in the interval [0.0, 1.0],
+        where larger values correspond to more similar strings or sequences of strings
 
     Raises:
         ValueError: if ``fuzzy_match`` is True but ``obj1`` and ``obj2`` are strings,
@@ -144,18 +150,18 @@ def jaccard(obj1, obj2, fuzzy_match=False, match_threshold=0.8):
     return intersection / union
 
 
-def levenshtein(str1, str2):
+def levenshtein(str1: str, str2: str) -> float:
     """
     Measure the similarity between two strings using Levenshtein distance, which
     gives the minimum number of character insertions, deletions, and substitutions
     needed to change one string into the other.
 
     Args:
-        str1 (str)
-        str2 (str)
+        str1
+        str2
 
     Returns:
-        float: similarity between ``str1`` and ``str2`` in the interval [0.0, 1.0],
+        Similarity between ``str1`` and ``str2`` in the interval [0.0, 1.0],
         where larger values correspond to more similar strings
     """
     distance = _levenshtein(str1, str2)
@@ -166,18 +172,18 @@ def levenshtein(str1, str2):
         return 0.0
 
 
-def token_sort_ratio(str1, str2):
+def token_sort_ratio(str1: str, str2: str) -> float:
     """
     Measure the similarity between two strings based on :func:`levenshtein()`,
     only with non-alphanumeric characters removed and the ordering of words
     in each string sorted before comparison.
 
     Args:
-        str1 (str)
-        str2 (str)
+        str1
+        str2
 
     Returns:
-        float: Similarity between ``str1`` and ``str2`` in the interval [0.0, 1.0],
+        Similarity between ``str1`` and ``str2`` in the interval [0.0, 1.0],
         where larger values correspond to more similar strings.
     """
     if not str1 or not str2:
@@ -187,7 +193,7 @@ def token_sort_ratio(str1, str2):
     return levenshtein(str1_proc, str2_proc)
 
 
-def _process_and_sort(s):
+def _process_and_sort(s: str) -> str:
     """
     Remove all characters from ``s`` except letters and numbers, strip whitespace,
     and force everything to lower-case; then sort tokens before re-joining into
@@ -198,18 +204,18 @@ def _process_and_sort(s):
     return " ".join(sorted(RE_ALNUM.findall(s.lower())))
 
 
-def character_ngrams(str1, str2):
+def character_ngrams(str1: str, str2: str) -> float:
     """
     Measure the similarity between two strings using a character ngrams similarity
     metric, in which strings are transformed into trigrams of alnum-only characters,
     vectorized and weighted by tf-idf, then compared by cosine similarity.
 
     Args:
-        str1 (str)
-        str2 (str)
+        str1
+        str2
 
     Returns:
-        float: Similarity between ``str1`` and ``str2`` in the interval [0.0, 1.0],
+        Similarity between ``str1`` and ``str2`` in the interval [0.0, 1.0],
         where larger values correspond to more similar strings
 
     Note:
