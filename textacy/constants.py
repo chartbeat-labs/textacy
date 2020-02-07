dd0b79a6a9c003888d3e99e10ec80d3609969e51
Burton DeWilde <burtdewilde@gmail.com>
2020-02-06 21:17:44 -0600

    Add type annotations to constants.py

--- textacy/constants.py
+++ textacy/constants.py
@@ -3,16 +3,17 @@ Collection of regular expressions and other (small, generally useful) constants.
 """
 import pathlib
 import re
+from typing import Dict, Pattern, Set
 
 
-DEFAULT_DATA_DIR = pathlib.Path(__file__).parent.resolve() / "data"
+DEFAULT_DATA_DIR: pathlib.Path = pathlib.Path(__file__).parent.resolve() / "data"
 
-NUMERIC_ENT_TYPES = {"ORDINAL", "CARDINAL", "MONEY", "QUANTITY", "PERCENT", "TIME", "DATE"}
-SUBJ_DEPS = {"agent", "csubj", "csubjpass", "expl", "nsubj", "nsubjpass"}
-OBJ_DEPS = {"attr", "dobj", "dative", "oprd"}
-AUX_DEPS = {"aux", "auxpass", "neg"}
+NUMERIC_ENT_TYPES: Set[str] = {"ORDINAL", "CARDINAL", "MONEY", "QUANTITY", "PERCENT", "TIME", "DATE"}
+SUBJ_DEPS: Set[str] = {"agent", "csubj", "csubjpass", "expl", "nsubj", "nsubjpass"}
+OBJ_DEPS: Set[str] = {"attr", "dobj", "dative", "oprd"}
+AUX_DEPS: Set[str] = {"aux", "auxpass", "neg"}
 
-REPORTING_VERBS = {
+REPORTING_VERBS: Set[str] = {
     "according", "accuse", "acknowledge", "add", "admit", "agree",
     "allege", "announce", "argue", "ask", "assert", "believe", "blame",
     "charge", "cite", "claim", "complain", "concede", "conclude",
@@ -24,9 +25,9 @@ REPORTING_VERBS = {
     "worry", "write"
 }
 
-MATCHER_VALID_OPS = {"!", "+", "?", "*"}
+MATCHER_VALID_OPS: Set[str] = {"!", "+", "?", "*"}
 
-POS_REGEX_PATTERNS = {
+POS_REGEX_PATTERNS: Dict[str, Dict[str, str]] = {
     "en": {
         "NP": r"<DET>? <NUM>* (<ADJ> <PUNCT>? <CONJ>?)* (<NOUN>|<PROPN> <PART>?)+",
         "PP": r"<ADP> <DET>? <NUM>* (<ADJ> <PUNCT>? <CONJ>?)* (<NOUN> <PART>?)+",
@@ -34,12 +35,12 @@ POS_REGEX_PATTERNS = {
     }
 }
 
-RE_MATCHER_TOKPAT_DELIM = re.compile(r"\s+")
-RE_MATCHER_SPECIAL_VAL = re.compile(
+RE_MATCHER_TOKPAT_DELIM: Pattern = re.compile(r"\s+")
+RE_MATCHER_SPECIAL_VAL: Pattern = re.compile(
     r"^(int|bool)\([^: ]+\)$",
     flags=re.UNICODE)
 
-RE_ACRONYM = re.compile(
+RE_ACRONYM: Pattern = re.compile(
     r"(?:^|(?<=\W))"
     r"(?:"
     r"(?:(?:(?:[A-Z]\.?)+[a-z0-9&/-]?)+(?:[A-Z][s.]?|\ds?))"
@@ -49,19 +50,19 @@ RE_ACRONYM = re.compile(
     r"(?:$|(?=\W))",
     flags=re.UNICODE)
 
-RE_LINEBREAK = re.compile(r"(\r\n|[\n\v])+")
-RE_NONBREAKING_SPACE = re.compile(r"[^\S\n\v]+", flags=re.UNICODE)
+RE_LINEBREAK: Pattern = re.compile(r"(\r\n|[\n\v])+")
+RE_NONBREAKING_SPACE: Pattern = re.compile(r"[^\S\n\v]+", flags=re.UNICODE)
 
 # regexes for cleaning up crufty terms
-RE_DANGLING_PARENS_TERM = re.compile(
+RE_DANGLING_PARENS_TERM: Pattern = re.compile(
     r"(?:\s|^)(\()\s{1,2}(.*?)\s{1,2}(\))(?:\s|$)", flags=re.UNICODE)
-RE_LEAD_TAIL_CRUFT_TERM = re.compile(
+RE_LEAD_TAIL_CRUFT_TERM: Pattern = re.compile(
     r"^[^\w(-]+|[^\w).!?]+$", flags=re.UNICODE)
-RE_LEAD_HYPHEN_TERM = re.compile(
+RE_LEAD_HYPHEN_TERM: Pattern = re.compile(
     r"^-([^\W\d_])", flags=re.UNICODE)
-RE_NEG_DIGIT_TERM = re.compile(
+RE_NEG_DIGIT_TERM: Pattern = re.compile(
     r"(-) (\d)", flags=re.UNICODE)
-RE_WEIRD_HYPHEN_SPACE_TERM = re.compile(
+RE_WEIRD_HYPHEN_SPACE_TERM: Pattern = re.compile(
     r"(?<=[^\W\d]) (-[^\W\d])", flags=re.UNICODE)
-RE_WEIRD_APOSTR_SPACE_TERM = re.compile(
+RE_WEIRD_APOSTR_SPACE_TERM: Pattern = re.compile(
     r"([^\W\d]+) ('[a-z]{1,2}\b)", flags=re.UNICODE)
