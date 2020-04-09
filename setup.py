a105d7569ffcb0e1c59be519050582d4d622e1d8
Burton DeWilde <burtdewilde@gmail.com>
2020-04-09 09:09:22 -0500

    Adopt pyproject.toml config standard
    and adapt setuptools configs to work with it

--- setup.py
+++ setup.py
@@ -1,17 +1,5 @@
-import io
-import os
+#!/usr/bin/env python
+import setuptools
 
-from setuptools import setup, find_packages
-
-
-about = {}
-root_path = os.path.abspath(os.path.dirname(__file__))
-with io.open(os.path.join(root_path, "textacy", "about.py")) as f:
-    exec(f.read(), about)
-
-# NOTE: Package configuration, including the name, metadata, and other options,
-# are set in the setup.cfg file.
-setup(
-    version=about["__version__"],
-    packages=find_packages(),
-)
+if __name__ == "__main__":
+    setuptools.setup()
