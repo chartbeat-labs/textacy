ba2c46ceaf815bf7d4d6b582745d92b16c19884e
Burton DeWilde <burtdewilde@gmail.com>
2020-08-13 22:02:59 -0500

    Roll back version move
    shit, this broke windows builds for some reason
    
    not having ANY luck with minor improvements tonight

--- textacy/__init__.py
+++ textacy/__init__.py
@@ -1,12 +1,11 @@
 import logging
 
+from textacy.about import __version__
 from textacy.constants import DEFAULT_DATA_DIR
 from textacy.corpus import Corpus
 from textacy.text_stats import TextStats
 from textacy.spacier.core import load_spacy_lang, make_spacy_doc
 from textacy.spacier.doc_extensions import set_doc_extensions
-
-__version__ = "0.10.0"
 
 set_doc_extensions()
 
