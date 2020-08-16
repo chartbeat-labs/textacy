9dfdb27357712317504a785f1d1eb390c947f0a6
Burton DeWilde <burtdewilde@gmail.com>
2020-08-16 15:50:23 -0500

    Add errors module for common error casses

---
+++ src/textacy/errors.py
@@ -0,0 +1,7 @@
+from typing import Any, Collection
+
+
+def value_not_valid(name: str, value: Any, valid_values: Collection[Any]):
+    raise ValueError(
+        f"{name} = {value} is invalid; must be one of {valid_values}."
+    )
