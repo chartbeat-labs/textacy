b39affb5e0183f7d9fb374cffbe25809f789a0a3
Burton DeWilde <burtdewilde@gmail.com>
2020-08-16 17:22:16 -0500

    Generate error msgs in errors module, don't raise

--- src/textacy/errors.py
+++ src/textacy/errors.py
@@ -1,5 +1,19 @@
-from typing import Any, Collection
+from typing import Any, Collection, Type
 
 
-def value_not_valid(name: str, value: Any, valid_values: Collection[Any]):
-    raise ValueError(f"{name} = {value} is invalid; must be one of {valid_values}.")
+def value_invalid_msg(
+    name: str,
+    value: Any,
+    valid_values: Collection[Any],
+) -> str:
+    return f"`{name}` value = {value} is invalid; value must be one of {valid_values}."
+
+
+def type_invalid_msg(
+    name: str,
+    val_type: Type,
+    valid_val_type: Type,
+) -> str:
+    return (
+        f"`{name}` type = {val_type} is invalid; type must match {valid_val_type}."
+    )
