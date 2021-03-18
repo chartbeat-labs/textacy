74b00fdaaf290fd2c62d46bb79fb9f97966ab9d0
Burton DeWilde <burtdewilde@gmail.com>
2021-03-18 16:10:41 -0500

    Rename arg name in preproc.replace
    it's shorter and matches the stdlib's re module, which is used under the hood. i figure this is slightly better.

--- src/textacy/preprocessing/replace.py
+++ src/textacy/preprocessing/replace.py
@@ -5,51 +5,48 @@ important as members of a group but not so much in the individual instances.
 from . import resources
 
 
-def replace_currency_symbols(text: str, replace_with: str = "_CUR_") -> str:
-    """Replace all currency symbols in ``text`` with ``replace_with``."""
-    return resources.RE_CURRENCY_SYMBOL.sub(replace_with, text)
+def replace_currency_symbols(text: str, repl: str = "_CUR_") -> str:
+    """Replace all currency symbols in ``text`` with ``repl``."""
+    return resources.RE_CURRENCY_SYMBOL.sub(repl, text)
 
 
-def replace_emails(text: str, replace_with: str = "_EMAIL_") -> str:
-    """Replace all email addresses in ``text`` with ``replace_with``."""
-    return resources.RE_EMAIL.sub(replace_with, text)
+def replace_emails(text: str, repl: str = "_EMAIL_") -> str:
+    """Replace all email addresses in ``text`` with ``repl``."""
+    return resources.RE_EMAIL.sub(repl, text)
 
 
-def replace_emojis(text: str, replace_with: str = "_EMOJI_") -> str:
+def replace_emojis(text: str, repl: str = "_EMOJI_") -> str:
     """
-    Replace all emoji and pictographs in ``text`` with ``replace_with``.
+    Replace all emoji and pictographs in ``text`` with ``repl``.
 
     Note:
         If your Python has a narrow unicode build ("USC-2"), only dingbats
         and miscellaneous symbols are replaced because Python isn't able
         to represent the unicode data for things like emoticons. Sorry!
     """
-    return resources.RE_EMOJI.sub(replace_with, text)
+    return resources.RE_EMOJI.sub(repl, text)
 
 
-def replace_hashtags(text: str, replace_with: str = "_TAG_") -> str:
-    """Replace all hashtags in ``text`` with ``replace_with``."""
-    return resources.RE_HASHTAG.sub(replace_with, text)
+def replace_hashtags(text: str, repl: str = "_TAG_") -> str:
+    """Replace all hashtags in ``text`` with ``repl``."""
+    return resources.RE_HASHTAG.sub(repl, text)
 
 
-def replace_numbers(text: str, replace_with: str = "_NUMBER_") -> str:
-    """Replace all numbers in ``text`` with ``replace_with``."""
-    return resources.RE_NUMBER.sub(replace_with, text)
+def replace_numbers(text: str, repl: str = "_NUMBER_") -> str:
+    """Replace all numbers in ``text`` with ``repl``."""
+    return resources.RE_NUMBER.sub(repl, text)
 
 
-def replace_phone_numbers(text: str, replace_with: str = "_PHONE_") -> str:
-    """Replace all phone numbers in ``text`` with ``replace_with``."""
-    return resources.RE_PHONE_NUMBER.sub(replace_with, text)
+def replace_phone_numbers(text: str, repl: str = "_PHONE_") -> str:
+    """Replace all phone numbers in ``text`` with ``repl``."""
+    return resources.RE_PHONE_NUMBER.sub(repl, text)
 
 
-def replace_urls(text: str, replace_with: str = "_URL_") -> str:
-    """Replace all URLs in ``text`` with ``replace_with``."""
-    return resources.RE_SHORT_URL.sub(
-        replace_with,
-        resources.RE_URL.sub(replace_with, text)
-    )
+def replace_urls(text: str, repl: str = "_URL_") -> str:
+    """Replace all URLs in ``text`` with ``repl``."""
+    return resources.RE_SHORT_URL.sub(repl, resources.RE_URL.sub(repl, text))
 
 
-def replace_user_handles(text: str, replace_with: str = "_USER_") -> str:
-    """Replace all user handles in ``text`` with ``replace_with``."""
-    return resources.RE_USER_HANDLE.sub(replace_with, text)
+def replace_user_handles(text: str, repl: str = "_USER_") -> str:
+    """Replace all (Twitter-style) user handles in ``text`` with ``repl``."""
+    return resources.RE_USER_HANDLE.sub(repl, text)
