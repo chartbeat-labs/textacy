a38dedf0393c2fdb62552f0b0599b5a6399d263a
Burton DeWilde <burtdewilde@gmail.com>
2021-03-22 17:29:51 -0500

    Yield SVOs per sentence rather than per doc

--- src/textacy/extract_/triples.py
+++ src/textacy/extract_/triples.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
 
 import collections
+import itertools
 from operator import attrgetter
 from typing import Iterable, List, Tuple
 
 from spacy.symbols import (
-    VERB, agent, aux, auxpass, csubj, csubjpass, dobj, neg, nsubj, nsubjpass, pobj, xcomp
+    CONJ, DET, VERB,
+    agent, aux, auxpass, csubj, csubjpass, dobj, neg, nsubj, nsubjpass, pobj, xcomp,
 )
 from spacy.tokens import Doc, Span, Token
 
@@ -19,7 +21,7 @@ def subject_verb_object_triples(
     doclike: Doc | Span,
 ) -> Iterable[Tuple[List[Token], List[Token], List[Token]]]:
     """
-    Extract an ordered sequence of subject-verb-object (SVO) triples from a document
+    Extract an ordered sequence of (subject, verb, object) triples from a document
     or sentence.
 
     Args:
@@ -27,16 +29,19 @@ def subject_verb_object_triples(
 
     Yields:
         Next SVO triple, in (more or less) order of appearance.
+
+    See Also:
+        :func:`semistructured_statements()`
     """
     if isinstance(doclike, Span):
         sents = [doclike]
     else:
         sents = doclike.sents
 
-    verb_sos = collections.defaultdict(lambda: collections.defaultdict(set))
-    # connect subjects/objects to direct verb heads
-    # and expand them to include conjuncts, compound nouns, ...
     for sent in sents:
+        # connect subjects/objects to direct verb heads
+        # and expand them to include conjuncts, compound nouns, ...
+        verb_sos = collections.defaultdict(lambda: collections.defaultdict(set))
         for tok in sent:
             head = tok.head
             # ensure entry for all verbs, even if empty
@@ -68,28 +73,28 @@ def subject_verb_object_triples(
                     # TODO: just the verb, or the whole tree?
                     # verb_sos[verb]["objects"].update(expand_verb(tok))
                     verb_sos[head]["objects"].update(tok.subtree)
-    # fill in any indirect relationships connected via verb conjuncts
-    for verb, so_dict in verb_sos.items():
-        conjuncts = verb.conjuncts
-        if so_dict.get("subjects"):
-            for conj in conjuncts:
-                conj_so_dict = verb_sos.get(conj)
-                if conj_so_dict and not conj_so_dict.get("subjects"):
-                    conj_so_dict["subjects"].update(so_dict["subjects"])
-        if not so_dict.get("objects"):
-            so_dict["objects"].update(
-                obj
-                for conj in conjuncts
-                for obj in verb_sos.get(conj, {}).get("objects", [])
-            )
-    # expand verbs and restructure into svo triples
-    for verb, so_dict in verb_sos.items():
-        if so_dict["subjects"] and so_dict["objects"]:
-            yield (
-                sorted(so_dict["subjects"], key=attrgetter("i")),
-                sorted(expand_verb(verb), key=attrgetter("i")),
-                sorted(so_dict["objects"], key=attrgetter("i")),
-            )
+        # fill in any indirect relationships connected via verb conjuncts
+        for verb, so_dict in verb_sos.items():
+            conjuncts = verb.conjuncts
+            if so_dict.get("subjects"):
+                for conj in conjuncts:
+                    conj_so_dict = verb_sos.get(conj)
+                    if conj_so_dict and not conj_so_dict.get("subjects"):
+                        conj_so_dict["subjects"].update(so_dict["subjects"])
+            if not so_dict.get("objects"):
+                so_dict["objects"].update(
+                    obj
+                    for conj in conjuncts
+                    for obj in verb_sos.get(conj, {}).get("objects", [])
+                )
+        # expand verbs and restructure into svo triples
+        for verb, so_dict in verb_sos.items():
+            if so_dict["subjects"] and so_dict["objects"]:
+                yield (
+                    sorted(so_dict["subjects"], key=attrgetter("i")),
+                    sorted(expand_verb(verb), key=attrgetter("i")),
+                    sorted(so_dict["objects"], key=attrgetter("i")),
+                )
 
 
 def expand_noun(tok: Token) -> List[Token]:
