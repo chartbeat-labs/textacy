a36d9c60d0b69a37e94a7e06466d10a4316d6b91
Burton DeWilde <burtdewilde@gmail.com>
2021-03-22 16:51:10 -0500

    Fix imports for renamed extract.triples

--- src/textacy/extract_/__init__.py
+++ src/textacy/extract_/__init__.py
@@ -1,3 +1,3 @@
 from .basics import words, ngrams, entities, noun_chunks
 from .kwic import keyword_in_context, KWIC
-from .svo import subject_verb_object_triples
+from .triples import subject_verb_object_triples
