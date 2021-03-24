32a5e80abb944794f0bcf5d7f64cd139e3a5b675
Burton DeWilde <burtdewilde@gmail.com>
2021-03-24 10:06:01 -0500

    Port over tests for token matches func

--- src/textacy/extract_/__init__.py
+++ src/textacy/extract_/__init__.py
@@ -2,6 +2,7 @@ from . import keyterms
 from .acronyms import acronyms_and_definitions
 from .basics import words, ngrams, entities, noun_chunks
 from .kwic import keyword_in_context, KWIC
+from .matches import token_matches, regex_matches
 from .triples import (
     direct_quotations, semistructured_statements, subject_verb_object_triples
 )
---
+++ tests/extract_/test_matches.py
@@ -0,0 +1,107 @@
+import re
+
+import pytest
+from spacy.tokens import Span
+
+from textacy import load_spacy_lang
+from textacy import extract_
+from textacy.extract_.matches import _make_pattern_from_string
+
+
+@pytest.fixture(scope="module")
+def spacy_doc():
+    spacy_lang = load_spacy_lang("en_core_web_sm")
+    text = (
+        "Two weeks ago, I was in Kuwait participating in an I.M.F. (International Monetary Fund) seminar for Arab educators. "
+        "For 30 minutes, we discussed the impact of technology trends on education in the Middle East. "
+        "And then an Egyptian education official raised his hand and asked if he could ask me a personal question: \"I heard Donald Trump say we need to close mosques in the United States,\" he said with great sorrow. "
+        "\"Is that what we want our kids to learn?\""
+    )
+    spacy_doc = spacy_lang(text)
+    return spacy_doc
+
+
+class TestTokenMatches:
+
+    def test_pattern_types(self, spacy_doc):
+        all_patterns = [
+            "POS:NOUN",
+            ["POS:NOUN", "POS:DET"],
+            [{"POS": "NOUN"}],
+            [[{"POS": "NOUN"}], [{"POS": "DET"}]],
+        ]
+        for patterns in all_patterns:
+            matches = list(extract_.token_matches(spacy_doc, patterns))[:5]
+            assert matches
+            assert all(isinstance(span, Span) for span in matches)
+
+    def test_patstr(self, spacy_doc):
+        matches = list(extract_.token_matches(spacy_doc, "POS:NOUN"))[:5]
+        assert matches
+        assert all(len(span) == 1 for span in matches)
+        assert all(span[0].pos_ == "NOUN" for span in matches)
+
+    def test_patstr_op(self, spacy_doc):
+        matches = list(extract_.token_matches(spacy_doc, "POS:NOUN:+"))[:5]
+        assert matches
+        assert all(len(span) >= 1 for span in matches)
+        assert all(tok.pos_ == "NOUN" for span in matches for tok in span)
+
+    def test_patstr_bool(self, spacy_doc):
+        matches = list(extract_.token_matches(spacy_doc, "IS_DIGIT:bool(True)"))[:5]
+        assert matches
+        assert all(span[0].is_digit is True for span in matches)
+
+    def test_patstr_int(self, spacy_doc):
+        matches = list(extract_.token_matches(spacy_doc, "LENGTH:int(5)"))[:5]
+        assert matches
+        assert all(len(span[0]) == 5 for span in matches)
+
+    def test_patdict(self, spacy_doc):
+        matches = list(extract_.token_matches(spacy_doc, [{"POS": "NOUN"}]))[:5]
+        assert matches
+        assert all(len(span) == 1 for span in matches)
+        assert all(span[0].pos_ == "NOUN" for span in matches)
+
+    def test_patdict_op(self, spacy_doc):
+        matches = list(extract_.token_matches(spacy_doc, [{"POS": "NOUN", "OP": "+"}]))[:5]
+        assert matches
+        assert all(len(span) >= 1 for span in matches)
+        assert all(tok.pos_ == "NOUN" for span in matches for tok in span)
+
+    def test_patdict_bool(self, spacy_doc):
+        matches = list(extract_.token_matches(spacy_doc, [{"IS_DIGIT": True}]))[:5]
+        assert matches
+        assert all(span[0].is_digit is True for span in matches)
+
+    def test_patdict_int(self, spacy_doc):
+        matches = list(extract_.token_matches(spacy_doc, [{"LENGTH": 5}]))[:5]
+        assert matches
+        assert all(len(span[0]) == 5 for span in matches)
+
+    def test_make_pattern_from_string(self):
+        patstr_to_pats = [
+            ("TAG:VBZ", [{"TAG": "VBZ"}]),
+            ("POS:NOUN:+", [{"POS": "NOUN", "OP": "+"}]),
+            ("IS_PUNCT:bool(False)", [{"IS_PUNCT": False}]),
+            (
+                "IS_DIGIT:bool(True):? POS:NOUN:*",
+                [{"IS_DIGIT": True, "OP": "?"}, {"POS": "NOUN", "OP": "*"}],
+            ),
+            (
+                "LENGTH:int(5) DEP:nsubj:!",
+                [{"LENGTH": 5}, {"DEP": "nsubj", "OP": "!"}],
+            ),
+            ("POS:DET :", [{"POS": "DET"}, {}]),
+            (
+                "IS_PUNCT:bool(False) : IS_PUNCT:bool(True)",
+                [{"IS_PUNCT": False}, {}, {"IS_PUNCT": True}],
+            ),
+        ]
+        for patstr, pat in patstr_to_pats:
+            assert _make_pattern_from_string(patstr) == pat
+
+    def test_make_pattern_from_str_error(self):
+        for patstr in ["POS", "POS:NOUN:VERB:+", "POS:NOUN:*?"]:
+            with pytest.raises(ValueError):
+                _ = _make_pattern_from_string(patstr)
