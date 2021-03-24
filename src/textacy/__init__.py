ec75d83b58de5d7309f95b0fcdf701b128fb93d1
Burton DeWilde <burtdewilde@gmail.com>
2021-03-24 17:36:48 -0500

    Add docs and pkg-level import for new lang id

--- src/textacy/__init__.py
+++ src/textacy/__init__.py
@@ -3,6 +3,7 @@ import logging
 from textacy.about import __version__
 from textacy.constants import DEFAULT_DATA_DIR
 from textacy.corpus import Corpus
+from textacy.lang_id import identify_lang
 from textacy.text_stats import TextStats
 from textacy.spacier.core import load_spacy_lang, make_spacy_doc
 from textacy.spacier.doc_extensions import set_doc_extensions
