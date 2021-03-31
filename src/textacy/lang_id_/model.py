971cd9fba4622fd05aacd537e8aeb3f6599e7504
Burton DeWilde <burtdewilde@gmail.com>
2021-03-30 22:06:11 -0500

    Add v2 lang id model implemented w/ thinc

---
+++ src/textacy/lang_id_/model.py
@@ -0,0 +1,148 @@
+from __future__ import annotations
+
+from typing import Callable, List, Optional, Sequence, Tuple
+
+import thinc
+from thinc.api import Model, chain, concatenate
+
+
+def LangIdentifierModelV2(
+    ns: Sequence[int] = (1, 2, 3),
+    embed_dim: int = 50,
+    hidden_width: int = 512,
+    dropout: Optional[float] = 0.1,
+) -> Model[List[str], thinc.types.Floats2d]:
+    """
+    Build a language identification model inspired by Google's CLD3.
+
+    Args:
+        ns: Set of "n" for which character "n"-grams are extracted from input texts.
+            If 1, only unigrams (single characters) are used; if [1, 2], then both
+            unigrams and bigrams are used; and so on.
+        embed_dim: Size of the vectors into which each set of ngrams are embedded.
+        hidden_width: Width of the dense layer with Relu activation, just before
+            the final prediction (Softmax) layer.
+        dropout: Dropout rate to avoid overfitting.
+
+    Returns:
+        Thinc :class:`Model`.
+    """
+    with Model.define_operators({">>": chain}):
+        model = (
+            MultiCharNgramsEmbedding(
+                ns=list(ns),
+                max_chars=1000,
+                num_vectors=[2000, 4000, 8000],
+                embed_dims=embed_dim,
+                dropout=dropout,
+            )
+            >> thinc.layers.Relu(
+                nI=embed_dim * len(ns),
+                nO=hidden_width,
+                dropout=dropout,
+            )
+            >> thinc.layers.Softmax(nI=hidden_width)
+        )
+    return model
+
+
+def MultiCharNgramsEmbedding(
+    ns: List[int],
+    max_chars: int | List[int],
+    num_vectors: int | List[int],
+    embed_dims: int | List[int],
+    dropout: Optional[float],
+) -> Model[List[str], thinc.types.Floats1d]:
+    """
+    Args:
+        ns
+        max_chars
+        num_vectors
+        embed_dims
+        dropout
+    """
+    numn = len(ns)
+    max_chars = [max_chars] * numn if isinstance(max_chars, int) else max_chars
+    num_vectors = [num_vectors] * numn if isinstance(num_vectors, int) else num_vectors
+    embed_dims = [embed_dims] * numn if isinstance(embed_dims, int) else embed_dims
+    with Model.define_operators({">>": chain}):
+        model = concatenate(*
+            [
+                CharNgramsEmbedding(
+                    n,
+                    max_chars=mc,
+                    num_vectors=nvec,
+                    embed_dim=edim,
+                    dropout=dropout,
+                )
+                for n, mc, nvec, edim in zip(ns, max_chars, num_vectors, embed_dims)
+            ]
+        )
+    return model
+
+
+def CharNgramsEmbedding(
+    n: int,
+    max_chars: int,
+    num_vectors: int,
+    embed_dim: int,
+    dropout: Optional[float],
+) -> Model[List[str], thinc.types.Floats1d]:
+    """
+    Args:
+        n
+        max_chars
+        num_vectors
+        embed_dim
+        dropout
+    """
+    with Model.define_operators({">>": chain}):
+        model = (
+            text_to_char_ngrams(n, max_chars)
+            >> thinc.layers.strings2arrays()
+            >> thinc.layers.with_array(
+                thinc.layers.HashEmbed(
+                    nO=embed_dim,
+                    nV=num_vectors,
+                    dropout=dropout,
+                    column=0,
+                )
+            )
+            >> thinc.layers.list2ragged()
+            >> thinc.layers.reduce_mean()
+        )
+    return model
+
+
+def text_to_char_ngrams(n: int, max_chars: int) -> Model[List[str], List[List[str]]]:
+    """
+    Custom data type transfer thinc layer that transforms a sequence of text strings
+    into a sequence of sequence of character ngram strings. Like this::
+
+        ["a short text.", "another text."] => [["a ", " s", "sh", "ho", ...], ...]
+
+    Args:
+        n: Number of adjacent characters to combine into an ngram.
+        max_chars: Max number of characters from the start of the text to transform
+            into character ngrams.
+    """
+
+    def forward(
+        model: Model, texts: List[str], is_train: bool
+    ) -> Tuple[List[List[str]], Callable]:
+        if n == 1:
+            char_ngs = [list(text[:max_chars]) for text in texts]
+        else:
+            char_ngs = [
+                [text[i : i + n] for i in range(min(len(text), max_chars) - n + 1)]
+                for text in texts
+            ]
+
+        def backprop(dY):
+            return []
+
+        return (char_ngs, backprop)
+
+    return Model(
+        "texts_to_char_ngrams", forward, attrs={"n": n, "max_chars": max_chars}
+    )
