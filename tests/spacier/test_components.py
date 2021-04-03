ae1863b062881a96307db545527f8cd11cf01f21
Burton DeWilde <burtdewilde@gmail.com>
2021-04-03 16:25:35 -0500

    Tweak text stats lang component

--- tests/spacier/test_components.py
+++ tests/spacier/test_components.py
@@ -1,5 +1,3 @@
-import os
-
 import pytest
 
 from textacy import load_spacy_lang
@@ -15,7 +13,7 @@ TEXT = (
 @pytest.fixture(scope="module")
 def spacy_lang():
     spacy_lang = load_spacy_lang("en_core_web_sm")
-    spacy_lang.add_pipe("textacy_text_stats", after="parser")
+    spacy_lang.add_pipe("textacy_text_stats", last=True)
 
     yield spacy_lang
 
@@ -33,15 +31,21 @@ def test_component_name(spacy_lang):
     assert spacy_lang.has_pipe("textacy_text_stats") is True
 
 
-def test_component_attrs():
-    attrs_args = [
+def test_component_problems(spacy_lang):
+    assert spacy_lang.analyze_pipes()["problems"]["textacy_text_stats"] == []
+
+
+@pytest.mark.parametrize(
+    "attrs",
+    [
         None,
         "flesch_kincaid_grade_level",
         ["flesch_kincaid_grade_level", "flesch_reading_ease"],
-    ]
-    for attrs in attrs_args:
-        text_stats_component = components.TextStatsComponent(attrs=attrs)
-        assert isinstance(text_stats_component.attrs, tuple) is True
+    ],
+)
+def test_component_attrs(attrs):
+    text_stats_component = components.TextStatsComponent(attrs=attrs)
+    assert isinstance(text_stats_component.attrs, tuple) is True
 
 
 def test_attrs_on_doc(spacy_lang, spacy_doc):
