cecb99234c0a66c7694308accd47ef51c8f65ef0
Burton DeWilde <burtdewilde@gmail.com>
2021-04-04 11:37:58 -0500

    Remove KWIC alias
    we don't do aliases like this anywhere else, and probably most users won't be familiar with that acronym. better to keep the api consistent and lean.

--- src/textacy/extract/__init__.py
+++ src/textacy/extract/__init__.py
@@ -1,7 +1,7 @@
 from . import keyterms
 from .acros import acronyms, acronyms_and_definitions
 from .basics import words, ngrams, entities, noun_chunks
-from .kwic import keyword_in_context, KWIC
+from .kwic import keyword_in_context
 from .matches import token_matches, regex_matches
 from .triples import (
     direct_quotations, semistructured_statements, subject_verb_object_triples
