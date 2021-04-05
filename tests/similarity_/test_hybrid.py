7f950fe66ae7da8523104e197b1f4f0b226adb70
Burton DeWilde <burtdewilde@gmail.com>
2021-04-05 15:32:54 -0500

    Add tests for hybrid similarity func

---
+++ tests/similarity_/test_hybrid.py
@@ -0,0 +1,29 @@
+import pytest
+
+from textacy.similarity_ import hybrid
+
+
+@pytest.mark.parametrize(
+    "s1, s2, exp",
+    [
+        ("abcd", "abcd", 1.0),
+        ("abdc", "wxyz", 0.0),
+        ("abcd", "", 0.0),
+        ("", "", 0.0),
+        ("abcd", "ABCD", 1.0),
+        ("abcd", "bacd", 0.5),
+        ("abcd", "abXcd", 0.8),
+        ("abcd", "abcdefgh", 0.5),
+        ("abcdefgh", "abcd", 0.5),
+        (list("abcd"), list("abcd"), 1.0),
+        (list("abcd"), list("ABCD"), 1.0),
+        ("abcd", "abcd efgh", 0.5),
+        (["abcd"], ["abcd", "efgh"], 0.5),
+        (["abcd", "wxyz"], ["abcd", "efgh"], 0.5),
+    ]
+)
+def test_token_sort_ratio(s1, s2, exp):
+    obs = hybrid.token_sort_ratio(s1, s2)
+    assert isinstance(obs, float)
+    assert 0.0 <= obs <= 1.0
+    assert obs == pytest.approx(exp, rel=0.01)
