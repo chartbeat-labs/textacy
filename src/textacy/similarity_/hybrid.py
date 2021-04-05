b05a8d2afcb4063ddf4ec272723bc9a74a85f1c4
Burton DeWilde <burtdewilde@gmail.com>
2021-04-05 15:33:39 -0500

    Tidy up some similarity bits

--- src/textacy/similarity_/hybrid.py
+++ src/textacy/similarity_/hybrid.py
@@ -19,13 +19,16 @@ def token_sort_ratio(s1: str | Sequence[str], s2: str | Sequence[str]) -> float:
     Returns:
         Similarity between ``s1`` and ``s2`` in the interval [0.0, 1.0],
         where larger values correspond to more similar strings.
+
+    See Also:
+        :func:`textacy.similarity_.edits.levenshtein()`
     """
-    str1 = _process_and_sort(s1)
-    str2 = _process_and_sort(s2)
+    str1 = _to_prepared_str(s1)
+    str2 = _to_prepared_str(s2)
     return edits.levenshtein(str1, str2)
 
 
-def _process_and_sort(s: str | Sequence[str]) -> str:
+def _to_prepared_str(s: str | Sequence[str]) -> str:
     """
     Remove all characters from ``s`` except letters and numbers, strip whitespace,
     and force everything to lower-case; then sort tokens before re-joining into
@@ -34,6 +37,6 @@ def _process_and_sort(s: str | Sequence[str]) -> str:
     tokens = (
         constants.RE_ALNUM.findall(s.lower())
         if isinstance(s, str)
-        else [tok.lower() for tok in s]
+        else [tok.lower().strip() for tok in s]
     )
-    return " ".join(sorted(tokens))
+    return "".join(sorted(tokens))
