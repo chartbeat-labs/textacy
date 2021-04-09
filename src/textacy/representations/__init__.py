211cfda3fc3d898774c4a31a26dfe6329c5eb48b
Burton DeWilde <burtdewilde@gmail.com>
2021-04-09 11:48:13 -0500

    Update reprs subpkg imports

--- src/textacy/representations/__init__.py
+++ src/textacy/representations/__init__.py
@@ -1,1 +1,13 @@
-from . import network
+from . import network, vectorizers
+from .network import build_cooccurrence_network, build_similarity_network
+from .vectorizers import Vectorizer, GroupVectorizer
+from .matrix_utils import (
+    get_term_freqs,
+    get_doc_freqs,
+    get_inverse_doc_freqs,
+    get_doc_lengths,
+    get_information_content,
+    apply_idf_weighting,
+    filter_terms_by_df,
+    filter_terms_by_ic,
+)
