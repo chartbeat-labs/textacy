e5526870a36c75907ff1b45c4901e8c34f70cc07
Burton DeWilde <burtdewilde@gmail.com>
2021-11-17 21:42:26 -0600

    Add and reorg types module

--- src/textacy/types.py
+++ src/textacy/types.py
@@ -2,13 +2,38 @@
 :mod:`textacy.types`: Definitions for common object types used throughout the package.
 """
 from pathlib import Path
-from typing import Any, Callable, Iterable, List, NamedTuple, Protocol, TypeVar, Union
+from typing import (
+    Any,
+    Callable,
+    Iterable,
+    List,
+    NamedTuple,
+    Protocol,
+    TypeVar,
+    Union,
+)
 
 from spacy.language import Language
 from spacy.tokens import Doc, Span, Token
 
 
 AnyVal = TypeVar("AnyVal")
+AnyStr = Union[str, bytes]
+
+PathLike = Union[str, Path]
+
+DocLike = Union[Doc, Span]
+SpanLike = Union[Span, Token]
+DocLikeToSpans = Callable[[DocLike], Iterable[Span]]
+
+LangLike = Union[str, Path, Language]
+LangLikeInContext = Union[
+    PathLike,
+    Language,
+    Callable[[str], str],
+    Callable[[str], Path],
+    Callable[[str], Language],
+]
 
 
 # typed equivalent to Record = collections.namedtuple("Record", ["text", "meta"])
@@ -19,23 +44,6 @@ class Record(NamedTuple):
 
 DocData = Union[str, Record, Doc]
 CorpusData = Union[str, Record, Doc, Iterable[str], Iterable[Record], Iterable[Doc]]
-
-LangLike = Union[str, Path, Language]
-LangLikeInContext = Union[
-    str,
-    Path,
-    Language,
-    Callable[[str], str],
-    Callable[[str], Path],
-    Callable[[str], Language],
-]
-
-DocLike = Union[Doc, Span]
-SpanLike = Union[Span, Token]
-
-PathLike = Union[str, Path]
-
-DocLikeToSpans = Callable[[DocLike], Iterable[Span]]
 
 
 # typed equivalent to AugTok = collections.namedtuple("AugTok", ["text", "ws", "pos", "is_word", "syns"])
