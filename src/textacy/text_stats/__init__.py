f8409d18a2f418169daf5fc888cfdeb1b574c48f
Burton DeWilde <burtdewilde@gmail.com>
2021-11-29 21:34:42 -0600

    Remove text stats custom lang component
    there's a better way

--- src/textacy/text_stats/__init__.py
+++ src/textacy/text_stats/__init__.py
@@ -1,3 +1,3 @@
 from .api import TextStats
 from .utils import load_hyphenator
-from . import basics, components, diversity, morph, readability, utils
+from . import basics, diversity, morph, readability, utils
