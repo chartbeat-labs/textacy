617112eccf07b5565a6a5b7c5613a2d1c2ca5a3e
Burton DeWilde <burtdewilde@gmail.com>
2021-11-30 20:35:11 -0600

    Delete superseded morph stats

--- src/textacy/text_stats/__init__.py
+++ src/textacy/text_stats/__init__.py
@@ -1,3 +1,3 @@
 from .api import TextStats, get_doc_extensions, remove_doc_extensions, set_doc_extensions
 from .utils import load_hyphenator
-from . import basics, diversity, morph, readability, utils
+from . import basics, counts, diversity, readability, utils
